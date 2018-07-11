0a8b08c29f07fcd5c434ffd4e872b2c7007f7611
torosent <torosent@microsoft.com>
2018-07-11 18:16:44 +0300

    Update name

--- presidio-analyzer/tests/test_credit_card.py
+++ presidio-analyzer/tests/test_credit_card.py
@@ -21,11 +21,3 @@ def test_invalid_credit_card():
     number = '4012-8888-8888-1882'
     results = match.analyze_text('my credit card number is ' + number, types)
     assert len(results) == 0
-
-
-def test_credit_card_text1():
-    path = os.path.dirname(__file__) + '/data/text1.txt'
-    text_file = open(path, 'r')
-    match = matcher.Matcher()
-    results = match.analyze_text(text_file.read(), types)
-    assert len(results) == 3
