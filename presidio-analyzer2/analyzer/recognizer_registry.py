10c84cb9fc40784ce86964921be1e4ff4104bfb8
Ilana Kantorov <ilanak@microsoft.com>
2019-01-27 19:46:11 +0200

    WIP

--- presidio-analyzer2/analyzer/recognizer_registry.py
+++ presidio-analyzer2/analyzer/recognizer_registry.py
@@ -2,15 +2,15 @@ import importlib
 import inspect
 import os
 import glob
-from recognizer import Recognizer
+from entity_recognizer import EntityRecognizer
 
 
 class RecognizerRegistry():
 
-    def load_models(self): 
+    def load_entity_recognizers(self):
         SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
         recognizer_directory_path = os.path.join(SCRIPT_DIR, 'recognizers')
-        return self.__load(recognizer_directory_path, base_class=Recognizer)
+        return self.__load(recognizer_directory_path, base_class=EntityRecognizer)
 
     def __load(self, plugins_package_directory_path, base_class=None, create_instance=True, filter_abstract=True):
 
@@ -42,12 +42,12 @@ class RecognizerRegistry():
                 
                 if not inspect.isclass(value):
                     continue
-                
+              
                 if filter_abstract and inspect.isabstract(value):
                     continue
                 print(value)
                 if base_class is not None:
-                    if type(value) != type(base_class):
+                    if isinstance(value) != type(base_class):
                         continue
 
                 # -----------------------------
@@ -58,5 +58,5 @@ class RecognizerRegistry():
                     result.append(value())
                 else:
                     result.append(value)
-            
+          
         return result
