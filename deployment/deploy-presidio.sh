69c36ef59680b0588f2b05eb3e501284ad79a9ce
Itye Richter <itye.richter@microsoft.com>
2019-02-06 14:38:41 +0200

    tag as parameter

--- deployment/deploy-presidio.sh
+++ deployment/deploy-presidio.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+REGISTRY=${1:-presidio.azurecr.io}
+TAG=${2:-latest}
 
+echo $REGISTRY
+echo $TAG
 helm install --name redis stable/redis --set usePassword=false,rbac.create=true --namespace presidio-system --wait
-helm install --name presidio-demo --set registry=$1,analyzer.tag=latest,anonymizer.tag=latest,anonymizerimage.tag=latest,ocr.tag=latest,scheduler.tag=latest,api.tag=latest,collector.tag=latest,datasink.tag=latest ../charts/presidio --namespace presidio
+helm install --name presidio-demo --set registry=$REGISTRY,analyzer.tag=$TAG,anonymizer.tag=$TAG,anonymizerimage.tag=$TAG,ocr.tag=$TAG,scheduler.tag=$TAG,api.tag=$TAG,collector.tag=$TAG,datasink.tag=$TAG ../charts/presidio --namespace presidio
