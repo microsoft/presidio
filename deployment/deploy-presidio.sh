5bce008f005d9d365868092d45a901f9069e59a1
Elad Iwanir <eladiw@users.noreply.github.com>
2019-02-06 15:39:01 +0200

    changing chart to support single tag for all images

--- deployment/deploy-presidio.sh
+++ deployment/deploy-presidio.sh
@@ -2,4 +2,4 @@
 REGISTRY=${1:-presidio.azurecr.io}
 TAG=${2:-latest}
 helm install --name redis stable/redis --set usePassword=false,rbac.create=true --namespace presidio-system --wait
-helm install --name presidio-demo --set registry=$REGISTRY,analyzer.tag=$TAG,anonymizer.tag=$TAG,anonymizerimage.tag=$TAG,ocr.tag=$TAG,scheduler.tag=$TAG,api.tag=$TAG,collector.tag=$TAG,datasink.tag=$TAG ../charts/presidio --namespace presidio
+helm install --name presidio-demo --set registry=$REGISTRY,tag=$TAG ../charts/presidio --namespace presidio
