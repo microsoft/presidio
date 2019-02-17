5ed4a1375d685778e52e5dce2b5900e1cf5076cb
Itye Richter <itye.richter@microsoft.com>
2019-02-17 23:06:31 +0200

    Presidio support for language code in template (#98)
    Configure a language code on the request level and not the field level. All requests should have one language

--- presidio-analyzer/analyzer/common_pb2.py
+++ presidio-analyzer/analyzer/common_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='common.proto',
   package='types',
   syntax='proto3',
-  serialized_pb=_b('\n\x0c\x63ommon.proto\x12\x05types\"B\n\nFieldTypes\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x14\n\x0clanguageCode\x18\x02 \x01(\t\x12\x10\n\x08minScore\x18\x03 \x01(\t\"q\n\rAnalyzeResult\x12\x0c\n\x04text\x18\x01 \x01(\t\x12 \n\x05\x66ield\x18\x02 \x01(\x0b\x32\x11.types.FieldTypes\x12\r\n\x05score\x18\x03 \x01(\x02\x12!\n\x08location\x18\x04 \x01(\x0b\x32\x0f.types.Location\"6\n\x08Location\x12\r\n\x05start\x18\x01 \x01(\x11\x12\x0b\n\x03\x65nd\x18\x02 \x01(\x11\x12\x0e\n\x06length\x18\x03 \x01(\x11*\x95\x02\n\x0e\x46ieldTypesEnum\x12\x0f\n\x0b\x43REDIT_CARD\x10\x00\x12\n\n\x06\x43RYPTO\x10\x01\x12\r\n\tDATE_TIME\x10\x02\x12\x0f\n\x0b\x44OMAIN_NAME\x10\x03\x12\x11\n\rEMAIL_ADDRESS\x10\x04\x12\r\n\tIBAN_CODE\x10\x05\x12\x0e\n\nIP_ADDRESS\x10\x06\x12\x07\n\x03NRP\x10\x07\x12\x0c\n\x08LOCATION\x10\x08\x12\n\n\x06PERSON\x10\t\x12\x10\n\x0cPHONE_NUMBER\x10\n\x12\x12\n\x0eUS_BANK_NUMBER\x10\x0b\x12\x15\n\x11US_DRIVER_LICENSE\x10\x0c\x12\x0b\n\x07US_ITIN\x10\r\x12\x0f\n\x0bUS_PASSPORT\x10\x0e\x12\n\n\x06US_SSN\x10\x0f\x12\n\n\x06UK_NHS\x10\x10\x62\x06proto3')
+  serialized_pb=_b('\n\x0c\x63ommon.proto\x12\x05types\",\n\nFieldTypes\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x10\n\x08minScore\x18\x02 \x01(\t\"q\n\rAnalyzeResult\x12\x0c\n\x04text\x18\x01 \x01(\t\x12 \n\x05\x66ield\x18\x02 \x01(\x0b\x32\x11.types.FieldTypes\x12\r\n\x05score\x18\x03 \x01(\x02\x12!\n\x08location\x18\x04 \x01(\x0b\x32\x0f.types.Location\"6\n\x08Location\x12\r\n\x05start\x18\x01 \x01(\x11\x12\x0b\n\x03\x65nd\x18\x02 \x01(\x11\x12\x0e\n\x06length\x18\x03 \x01(\x11\"a\n\x05Image\x12\x0c\n\x04\x64\x61ta\x18\x01 \x01(\x0c\x12\x11\n\timageType\x18\x02 \x01(\t\x12)\n\rBoundingboxes\x18\x03 \x03(\x0b\x32\x12.types.Boundingbox\x12\x0c\n\x04text\x18\x04 \x01(\t\"\x8c\x01\n\x0b\x42oundingbox\x12\x11\n\txLocation\x18\x01 \x01(\x02\x12\r\n\x05width\x18\x02 \x01(\x02\x12\x11\n\tyLocation\x18\x03 \x01(\x02\x12\x0e\n\x06height\x18\x04 \x01(\x02\x12\x0c\n\x04text\x18\x05 \x01(\t\x12\x15\n\rstartPosition\x18\x06 \x01(\x11\x12\x13\n\x0b\x65ndPosition\x18\x07 \x01(\x11*\x95\x02\n\x0e\x46ieldTypesEnum\x12\x0f\n\x0b\x43REDIT_CARD\x10\x00\x12\n\n\x06\x43RYPTO\x10\x01\x12\r\n\tDATE_TIME\x10\x02\x12\x0f\n\x0b\x44OMAIN_NAME\x10\x03\x12\x11\n\rEMAIL_ADDRESS\x10\x04\x12\r\n\tIBAN_CODE\x10\x05\x12\x0e\n\nIP_ADDRESS\x10\x06\x12\x07\n\x03NRP\x10\x07\x12\x0c\n\x08LOCATION\x10\x08\x12\n\n\x06PERSON\x10\t\x12\x10\n\x0cPHONE_NUMBER\x10\n\x12\x12\n\x0eUS_BANK_NUMBER\x10\x0b\x12\x15\n\x11US_DRIVER_LICENSE\x10\x0c\x12\x0b\n\x07US_ITIN\x10\r\x12\x0f\n\x0bUS_PASSPORT\x10\x0e\x12\n\n\x06US_SSN\x10\x0f\x12\n\n\x06UK_NHS\x10\x10*;\n\x11\x44\x65tectionTypeEnum\x12\x07\n\x03OCR\x10\x00\x12\r\n\tAZURE_OCR\x10\x01\x12\x0e\n\nAZURE_FACE\x10\x02\x62\x06proto3')
 )
 
 _FIELDTYPESENUM = _descriptor.EnumDescriptor(
@@ -100,12 +100,39 @@ _FIELDTYPESENUM = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=263,
-  serialized_end=540,
+  serialized_start=483,
+  serialized_end=760,
 )
 _sym_db.RegisterEnumDescriptor(_FIELDTYPESENUM)
 
 FieldTypesEnum = enum_type_wrapper.EnumTypeWrapper(_FIELDTYPESENUM)
+_DETECTIONTYPEENUM = _descriptor.EnumDescriptor(
+  name='DetectionTypeEnum',
+  full_name='types.DetectionTypeEnum',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='OCR', index=0, number=0,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='AZURE_OCR', index=1, number=1,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='AZURE_FACE', index=2, number=2,
+      options=None,
+      type=None),
+  ],
+  containing_type=None,
+  options=None,
+  serialized_start=762,
+  serialized_end=821,
+)
+_sym_db.RegisterEnumDescriptor(_DETECTIONTYPEENUM)
+
+DetectionTypeEnum = enum_type_wrapper.EnumTypeWrapper(_DETECTIONTYPEENUM)
 CREDIT_CARD = 0
 CRYPTO = 1
 DATE_TIME = 2
@@ -123,6 +150,9 @@ US_ITIN = 13
 US_PASSPORT = 14
 US_SSN = 15
 UK_NHS = 16
+OCR = 0
+AZURE_OCR = 1
+AZURE_FACE = 2
 
 
 
@@ -141,15 +171,8 @@ _FIELDTYPES = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='languageCode', full_name='types.FieldTypes.languageCode', index=1,
+      name='minScore', full_name='types.FieldTypes.minScore', index=1,
       number=2, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=_b("").decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='minScore', full_name='types.FieldTypes.minScore', index=2,
-      number=3, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -167,7 +190,7 @@ _FIELDTYPES = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=23,
-  serialized_end=89,
+  serialized_end=67,
 )
 
 
@@ -218,8 +241,8 @@ _ANALYZERESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=91,
-  serialized_end=204,
+  serialized_start=69,
+  serialized_end=182,
 )
 
 
@@ -263,16 +286,145 @@ _LOCATION = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=206,
-  serialized_end=260,
+  serialized_start=184,
+  serialized_end=238,
+)
+
+
+_IMAGE = _descriptor.Descriptor(
+  name='Image',
+  full_name='types.Image',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='data', full_name='types.Image.data', index=0,
+      number=1, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b(""),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='imageType', full_name='types.Image.imageType', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='Boundingboxes', full_name='types.Image.Boundingboxes', index=2,
+      number=3, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='text', full_name='types.Image.text', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=240,
+  serialized_end=337,
+)
+
+
+_BOUNDINGBOX = _descriptor.Descriptor(
+  name='Boundingbox',
+  full_name='types.Boundingbox',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='xLocation', full_name='types.Boundingbox.xLocation', index=0,
+      number=1, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='width', full_name='types.Boundingbox.width', index=1,
+      number=2, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='yLocation', full_name='types.Boundingbox.yLocation', index=2,
+      number=3, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='height', full_name='types.Boundingbox.height', index=3,
+      number=4, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='text', full_name='types.Boundingbox.text', index=4,
+      number=5, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='startPosition', full_name='types.Boundingbox.startPosition', index=5,
+      number=6, type=17, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='endPosition', full_name='types.Boundingbox.endPosition', index=6,
+      number=7, type=17, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=340,
+  serialized_end=480,
 )
 
 _ANALYZERESULT.fields_by_name['field'].message_type = _FIELDTYPES
 _ANALYZERESULT.fields_by_name['location'].message_type = _LOCATION
+_IMAGE.fields_by_name['Boundingboxes'].message_type = _BOUNDINGBOX
 DESCRIPTOR.message_types_by_name['FieldTypes'] = _FIELDTYPES
 DESCRIPTOR.message_types_by_name['AnalyzeResult'] = _ANALYZERESULT
 DESCRIPTOR.message_types_by_name['Location'] = _LOCATION
+DESCRIPTOR.message_types_by_name['Image'] = _IMAGE
+DESCRIPTOR.message_types_by_name['Boundingbox'] = _BOUNDINGBOX
 DESCRIPTOR.enum_types_by_name['FieldTypesEnum'] = _FIELDTYPESENUM
+DESCRIPTOR.enum_types_by_name['DetectionTypeEnum'] = _DETECTIONTYPEENUM
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 FieldTypes = _reflection.GeneratedProtocolMessageType('FieldTypes', (_message.Message,), dict(
@@ -295,6 +447,20 @@ Location = _reflection.GeneratedProtocolMessageType('Location', (_message.Messag
   # @@protoc_insertion_point(class_scope:types.Location)
   ))
 _sym_db.RegisterMessage(Location)
+
+Image = _reflection.GeneratedProtocolMessageType('Image', (_message.Message,), dict(
+  DESCRIPTOR = _IMAGE,
+  __module__ = 'common_pb2'
+  # @@protoc_insertion_point(class_scope:types.Image)
+  ))
+_sym_db.RegisterMessage(Image)
+
+Boundingbox = _reflection.GeneratedProtocolMessageType('Boundingbox', (_message.Message,), dict(
+  DESCRIPTOR = _BOUNDINGBOX,
+  __module__ = 'common_pb2'
+  # @@protoc_insertion_point(class_scope:types.Boundingbox)
+  ))
+_sym_db.RegisterMessage(Boundingbox)
 
 
 # @@protoc_insertion_point(module_scope)
