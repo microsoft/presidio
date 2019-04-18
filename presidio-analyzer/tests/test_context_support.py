289b0836ae8bcce13974816f0bdbe38979316be1
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2019-04-18 13:10:40 +0300

    adding one context sentence with a lot of words

--- presidio-analyzer/tests/test_context_support.py
+++ presidio-analyzer/tests/test_context_support.py
@@ -62,8 +62,8 @@ def sentences_with_context(request):
         test_items.append((lines[i+1].strip(),
                            recognizer,
                            [lines[i].strip()]))
-    # Currently we have 19 sentences, this is a sanity
-    if not len(test_items) == 19:
+    # Currently we have 20 sentences, this is a sanity
+    if not len(test_items) == 20:
         raise ValueError("context sentences not as expected")
 
     request.cls.context_sentences = test_items
