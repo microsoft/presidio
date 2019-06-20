56c27c35b391d8e2b0723ca142f6f58784b605d0
Itye Richter <ityer@ityer.local>
2019-06-20 15:06:25 +0300

    seprated 2 loggers, and refactored code to apply

---
+++ presidio-analyzer/analyzer/app_tracer.py
@@ -0,0 +1,29 @@
+import logging
+
+
+class AppTracer:
+"""This class handles app traces"""
+    def __init__(self, enable_interpretability=True):
+
+        logger = logging.getLogger('Interpretability')
+        if not logger.handlers:
+            ch = logging.StreamHandler()
+            formatter = logging.Formatter(
+                '[%(asctime)s][%(name)s][%(levelname)s]%(message)s')
+            ch.setFormatter(formatter)
+            logger.addHandler(ch)
+            logger.setLevel(logging.INFO)
+            logger.propagate = False
+
+        self.logger = logger
+        self.enable_interpretability = enable_interpretability
+
+    def trace(self, request_id, trace_data):
+        """
+        Writes interpretability trace
+        :param request_id: A unique ID, to correlate across calls.
+        :param trace_data: A string to write.
+        :return:
+        """
+        if self.enable_interpretability:
+            self.logger.info("[%s][%s]", request_id, trace_data)
---
+++ presidio-analyzer/tests/mocks/app_tracer_mock.py
@@ -0,0 +1,34 @@
+import logging
+
+
+class MLTracerMock:
+
+    def __init__(self, enable_interpretability=True):
+
+        logger = logging.getLogger('InterpretabilityMock')
+        if not logger.handlers:
+            ch = logging.StreamHandler()
+            formatter = logging.Formatter(
+                '[%(asctime)s][%(name)s][%(levelname)s]%(message)s')
+            ch.setFormatter(formatter)
+            logger.addHandler(ch)
+            logger.setLevel(logging.INFO)
+            logger.propagate = False
+
+        self.logger = logger
+        self.last_trace = None
+        self.enable_interpretability = enable_interpretability
+
+    def trace(self, request_id, trace_data):
+        """
+        Writes interpretability trace
+        :param request_id: A unique ID, to correlate across calls.
+        :param trace_data: A string to write.
+        :return:
+        """
+        if self.enable_interpretability:
+            self.last_trace = "[{}][{}]".format(request_id, trace_data)
+            self.logger.info("[%s][%s]", request_id, trace_data)
+
+    def get_last_trace(self):
+        return self.last_trace
