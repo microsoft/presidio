6af72d2ac5fe8d3551b84c25e66621c2768f8f45
Avishay balter <avishay.balter@gmail.com>
2019-07-02 09:38:21 +0300

    fix gh commit

--- presidio-analyzer/analyzer/analyzer_engine.py
+++ presidio-analyzer/analyzer/analyzer_engine.py
@@ -14,8 +14,7 @@ DEFAULT_LANGUAGE = "en"
 
 class AnalyzerEngine(analyze_pb2_grpc.AnalyzeServiceServicer):
 
-    def __init__(self, registry=None, nlp_engine=None)
-                 nlp_engine):
+    def __init__(self, registry=None, nlp_engine=None):
         if not nlp_engine:
             from analyzer.nlp_engine import SpacyNlpEngine 
             nlp_engine = SpacyNlpEngine()
