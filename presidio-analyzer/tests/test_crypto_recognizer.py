b2e91108fe5a128c16733bd7064d560767a15ffd
rabee333 <rabeea.abusaleh@gmail.com>
2020-01-13 12:17:36 +0200

    Fix crypto_recognizer throws an exception #241

--- presidio-analyzer/tests/test_crypto_recognizer.py
+++ presidio-analyzer/tests/test_crypto_recognizer.py
@@ -21,14 +21,19 @@ class TestCreditCardRecognizer(TestCase):
 
     def test_valid_btc_with_exact_context(self):
         wallet = '16Yeky6GMjeNkAiNcBY7ZhrLoMSgg1BoyZ'
-        context = 'my wallet address is: '
-        results = crypto_recognizer.analyze(context + wallet, entities)
+        results = crypto_recognizer.analyze('my wallet address is: ' + wallet, entities)
 
         assert len(results) == 1
         assert_result(results[0], entities[0], 22, 56, EntityRecognizer.MAX_SCORE)
 
     def test_invalid_btc(self):
         wallet = '16Yeky6GMjeNkAiNcBY7ZhrLoMSgg1BoyZ2'
+        results = crypto_recognizer.analyze('my wallet address is ' + wallet, entities)
+
+        assert len(results) == 0
+
+    def test_invalid_btc_chars(self):
+        wallet = '34e7b5e1a0aa1d6f3d862b52a289cdb7'
         results = crypto_recognizer.analyze('my wallet address is ' + wallet, entities)
 
         assert len(results) == 0
