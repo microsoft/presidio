5436e254e7a9baf0f8b6add49bba4a9e11113929
Rabeea Abu-Saleh <rabeea.abusaleh@gmail.com>
2020-01-14 15:05:09 +0200

    Crypto analyzer fix 241 (#259)

--- presidio-analyzer/analyzer/predefined_recognizers/crypto_recognizer.py
+++ presidio-analyzer/analyzer/predefined_recognizers/crypto_recognizer.py
@@ -4,7 +4,7 @@ from analyzer import PatternRecognizer
 
 # Copied from:
 # http://rosettacode.org/wiki/Bitcoin/address_validation#Python
-REGEX = r'\b[13][a-km-zA-HJ-NP-Z0-9]{26,33}\b'
+REGEX = r'\b[13][a-km-zA-HJ-NP-Z1-9]{26,33}\b'
 CONTEXT = ["wallet", "btc", "bitcoin", "crypto"]
 
 
@@ -19,11 +19,12 @@ class CryptoRecognizer(PatternRecognizer):
                          context=CONTEXT)
 
     def validate_result(self, pattern_text):
-        # try:
-        bcbytes = CryptoRecognizer.__decode_base58(pattern_text, 25)
-        result = bcbytes[-4:] == sha256(sha256(bcbytes[:-4])
-                                        .digest()).digest()[:4]
-        return result
+        try:
+            bcbytes = CryptoRecognizer.__decode_base58(pattern_text, 25)
+            return bcbytes[-4:] == sha256(sha256(bcbytes[:-4])
+                                          .digest()).digest()[:4]
+        except ValueError:
+            return False
 
     @staticmethod
     def __decode_base58(bc, length):
