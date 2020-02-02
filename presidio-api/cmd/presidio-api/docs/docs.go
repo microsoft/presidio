fa62fb89dab6064f601275659823c5fb32b2e4ea
rabee333 <rabeea.abusaleh@gmail.com>
2020-02-02 09:08:52 +0000

    fix meta

--- presidio-api/cmd/presidio-api/docs/docs.go
+++ presidio-api/cmd/presidio-api/docs/docs.go
@@ -2,22 +2,15 @@
 //
 // Documentation for Presidio API Swagger.
 //
-//     Schemes: http
+//     Schemes: http, https
 //     BasePath: /api/v1
-//     Version: 1.0.0
+//     Version: 1.0
 //
 //     Consumes:
 //     - application/json
 //
 //     Produces:
 //     - application/json
-//
-//     Security:
-//     - basic
-//
-//    SecurityDefinitions:
-//    basic:
-//      type: basic
 //
 // swagger:meta
 package docs
