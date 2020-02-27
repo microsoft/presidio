1a49f8a66cbdf9a83bd5f126c1e769accd6b9eed
Avishay <avishay.balter@gmail.com>
2020-02-27 15:18:27 +0200

    init

--- presidio-analyzer/analyzer/__init__.py
+++ presidio-analyzer/analyzer/__init__.py
@@ -1,19 +1,20 @@
-import os
-import sys
+import os # noqa
+import sys # noqa
 
 # pylint: disable=unused-import,wrong-import-position
 sys.path.append(os.path.dirname(os.path.dirname(
-    os.path.abspath(__file__))) + "/analyzer")
-from analyzer.presidio_logger import PresidioLogger # noqa
-from analyzer.analysis_explanation import AnalysisExplanation # noqa
-from analyzer.pattern import Pattern # noqa
-from analyzer.entity_recognizer import EntityRecognizer # noqa
-from analyzer.local_recognizer import LocalRecognizer # noqa
-from analyzer.recognizer_result import RecognizerResult # noqa
-from analyzer.pattern_recognizer import PatternRecognizer # noqa
-from analyzer.remote_recognizer import RemoteRecognizer # noqa
-from analyzer.recognizer_registry.recognizer_registry import RecognizerRegistry # noqa
-from analyzer.analyzer_engine import AnalyzerEngine # noqa
+    os.path.abspath(__file__))) + "/analyzer") # noqa
+
+from analyzer.presidio_logger import PresidioLogger
+from analyzer.analysis_explanation import AnalysisExplanation
+from analyzer.pattern import Pattern
+from analyzer.entity_recognizer import EntityRecognizer
+from analyzer.local_recognizer import LocalRecognizer
+from analyzer.recognizer_result import RecognizerResult
+from analyzer.pattern_recognizer import PatternRecognizer
+from analyzer.remote_recognizer import RemoteRecognizer
+from analyzer.recognizer_registry.recognizer_registry import RecognizerRegistry
+from analyzer.analyzer_engine import AnalyzerEngine
 
 
 __all__ = ['PresidioLogger', 'AnalysisExplanation', 'Pattern',
