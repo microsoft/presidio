881fbd85f8bc90dc9e8d1602b742abbf7c5a8347
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-03-08 20:18:57 +0200

    adding docs

---
+++ deployment/run-with-kind.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script utilize KIND (https://github.com/kubernetes-sigs/kind), to install Presidio
+# into docker (with k8s emulation).
+# This script is intended to get Presidio up and running in no time, so you can play with it.
+# However it is NOT intended to be installed in this manner for any workload (dev or prod).
+
+# Clone KIND and build it (this way we eliminate the need to have GO installed)
+it clone https://github.com/kubernetes-sigs/kind.git
+cd kind
+make build
+
+# Create a KIND cluster into which we will deploy Presidio
+kind create cluster
+
+# Create a namespace
+kubectl create namespace presidio
+
+# Install Presidio
+cd ..
+./deploy-helm.sh
+./deploy-presidio.sh
+
