199af228ecbff5a3fbc6d7e2fc569bcc9119b703
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-03-14 22:25:48 +0200

    adding verbose log prints

--- deployment/prerequisites.sh
+++ deployment/prerequisites.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Install make
+echo "Installing make"
 apt install make
 
 
 # Install Docker CE
+echo "Installing Docker CE"
 apt-get update
 
 apt-get install \
@@ -26,6 +28,7 @@ apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 
 # Install kubectl
+echo "Installing kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 
 chmod +x ./kubectl
@@ -35,6 +38,7 @@ mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client
 
 # Install Helm
+echo "Installing Helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get
 
 chmod 700 get_helm.sh
