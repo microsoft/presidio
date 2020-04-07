b292cb655e6e6a10c295f1b0b0f1003110ce7d8a
David Pollack <d.pollack@solvemate.com>
2020-04-07 14:41:48 +0200

    __init__.py method for protobuf files

--- presidio-analyzer/presidio_analyzer/protobuf_models/scan_pb2.py
+++ presidio-analyzer/presidio_analyzer/protobuf_models/scan_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 _sym_db = _symbol_database.Default()
 
 
-from . import template_pb2 as template__pb2
+import template_pb2 as template__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
