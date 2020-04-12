191b43476dfeffcebf934b71f833369156be40e1
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-04-12 13:46:34 +0300

    updating doc

--- presidio-analyzer/presidio_analyzer/predefined_recognizers/il_id_recognizer.py
+++ presidio-analyzer/presidio_analyzer/predefined_recognizers/il_id_recognizer.py
@@ -2,9 +2,11 @@ from presidio_analyzer import Pattern
 from presidio_analyzer import PatternRecognizer
 # pylint: disable=line-too-long,abstract-method
 
+# Note: this recognizer is very simple and act mainly as an example on how to add new language support
+
 ISRAELI_ID_REGEX = r'\b[0-9]{7,10}\b'
 
-# Todo (Task: TBD): fix context for unsupported languages
+# Known issue (https://github.com/microsoft/presidio/issues/303) fix context for unsupported languages
 CONTEXT = [
     "תעודה",
     "זהות",
