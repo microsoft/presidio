9b944d15a960a14592c84f9125384da048b2f193
shiranrubin <shiranrubin@microsoft.com>
2021-01-12 10:16:14 +0200

    2608 - Cookiecutter for anonymizer.

---
+++ presidio-anonymizer/presidio_anonymizer/tests/test_presidio_anonymizer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Tests for `presidio_anonymizer` package."""
+
+import pytest
+
+
+from presidio_anonymizer import presidio_anonymizer
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
