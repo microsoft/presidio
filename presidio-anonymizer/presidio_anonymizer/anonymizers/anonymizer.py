78f522dfb9eeeaab2c2df8996651303847b084c4
Shiran Rubin <shiranrubin@gmail.com>
2021-01-17 14:43:02 +0200

    Update presidio-anonymizer/presidio_anonymizer/anonymizers/anonymizer.py
    Co-authored-by: Omri Mendels <omri374@users.noreply.github.com>

--- presidio-anonymizer/presidio_anonymizer/anonymizers/anonymizer.py
+++ presidio-anonymizer/presidio_anonymizer/anonymizers/anonymizer.py
@@ -1,6 +1,10 @@
 import abc
 
 
-class Anonymizer(metaclass=abc.ABCMeta):
+from abc import abstractmethod, ABC
+class Anonymizer(ABC):
+    @abstractmethod
+    def anonymize(self):
+         pass
     def anonymize(self):
         pass
