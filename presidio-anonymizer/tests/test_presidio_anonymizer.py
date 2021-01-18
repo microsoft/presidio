8b5928eee96eca9e1fcb7769879a2890459c5224
sharon <sharon.hart@microsoft.com>
2021-01-18 14:10:20 +0200

    remove test docs, add story number

--- presidio-anonymizer/tests/test_presidio_anonymizer.py
+++ presidio-anonymizer/tests/test_presidio_anonymizer.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python
-"""Anonymizer routes tests."""
 import api
 
 
 def test_anonymize():
-    """
-    Test anonymize method.
 
-    :return:
-    """
     tester = api.app.test_client()
     resp = tester.post("/anonymize", json={"hello": 1, "world": "what?"})
     assert resp.json == {"hello": 1, "world": "what?"}
