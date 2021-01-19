0dab17ab52aea7283a078095dde3b679881fc403
Nava Vaisman Levy <navalev@users.noreply.github.com>
2021-01-19 17:16:47 +0200

    Revert "add inclusive linting"
    This reverts commit 074983242eaf9a0025ea2f704f90f440b6ecc2b8.

--- node_modules/clear/examples/delay-clear.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-var clear = require('..'),
-    delay = 4; // seconds
-
-console.log('Screen will clear in %d seconds', delay);
-setTimeout(function() {
-  clear();
-}, delay * 1000);
