1424c8ee3f2e1f5ae26481e06fb0bcb633421320
shiranrubin <shiranrubin@microsoft.com>
2021-01-21 11:54:14 +0200

    Code review changes

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -1,27 +1,35 @@
+import os
+
 from flask import Flask, request
 
 from presidio_anonymizer.anonymizer_engine import AnonymizerEngine
 from presidio_anonymizer.entities.anonymizer_request import AnonymizerRequest
 from presidio_anonymizer.entities.invalid_exception import InvalidParamException
 
-app = Flask(__name__)
+DEFAULT_PORT = "3000"
 
 
-@app.route("/anonymize", methods=["POST"])
-def anonymize():
-    content = request.get_json()
-    if not content:
-        return "Invalid request json", 400
-    try:
-        data = AnonymizerRequest(content)
-        text = AnonymizerEngine().anonymize(data)
-    except InvalidParamException as e:
-        return e.err, 400
-    except Exception:
-        # TODO add logger 2652
-        return "Internal server error", 500
-    return text
+class Server:
+    def __init__(self):
+        self.app = Flask(__name__)
+
+        @self.app.route("/anonymize", methods=["POST"])
+        def anonymize():
+            content = request.get_json()
+            if not content:
+                return "Invalid request json", 400
+            try:
+                data = AnonymizerRequest(content)
+                text = AnonymizerEngine().anonymize(data)
+            except InvalidParamException as e:
+                return e.err, 400
+            except Exception:
+                # TODO add logger 2652
+                return "Internal server error", 500
+            return text
 
 
-if __name__ == "main":
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", DEFAULT_PORT))
+    server = Server()
+    server.app.run(port=port)
--- presidio-anonymizer/tests/test_presidio_app.py
+++ presidio-anonymizer/tests/test_presidio_app.py
@@ -1,13 +1,13 @@
 import json
 import os
 
-import app
+from app import Server
 
 
 def test_anonymize_api_works_properly():
     json_path = os.path.dirname(__file__) + "/resources/payload.json"
     with open(json_path) as json_file:
         content = json.load(json_file)
-    tester = app.app.test_client()
+    tester = Server().app.test_client()
     resp = tester.post("/anonymize", json=content)
     assert resp.status == "200 OK"
