c5484fbadf170e97274de1e37208896837118093
sharon <sharon.hart@microsoft.com>
2021-01-28 12:13:19 +0200

    Add validators helper method

---
+++ presidio-anonymizer/presidio_anonymizer/anonymizers/validators.py
@@ -0,0 +1,20 @@
+import logging
+
+from presidio_anonymizer.entities import InvalidParamException
+
+logger = logging.getLogger("presidio-anonymizer")
+
+
+def validate_parameter(
+    parameter_value, parameter_name: str, parameter_type: type
+) -> None:
+    """Validate an anonymizer parameter.
+
+    Both validate a parameter exists and that it is an instance of the
+    parameter_type. Otherwise, raise the appropriate InvalidParamException
+    with the parameter_name as content.
+    """
+    if not parameter_value:
+        raise InvalidParamException(f"Expected parameter {parameter_name}")
+    if not isinstance(parameter_value, parameter_type):
+        raise InvalidParamException(f"Invalid parameter {parameter_name}")
