3246236cda2ccf31949a6b1715c891e1918348c4
sharon <sharon.hart@microsoft.com>
2021-01-28 17:50:08 +0200

    Add typed exception message, moved mask parameter names to constants

--- presidio-anonymizer/tests/anonymizers/test_validators.py
+++ presidio-anonymizer/tests/anonymizers/test_validators.py
@@ -4,7 +4,7 @@ from presidio_anonymizer.entities import InvalidParamException
 from presidio_anonymizer.anonymizers.validators import validate_parameter
 
 
-def test_validate_parameter_when_parameter_is_none_typed_ipe_raised():
+def test_when_parameter_is_none_typed_then_ipe_raised():
 
     with pytest.raises(InvalidParamException, match="Expected parameter name"):
         validate_parameter(
@@ -12,11 +12,22 @@ def test_validate_parameter_when_parameter_is_none_typed_ipe_raised():
         )
 
 
-def test_validate_parameter_when_parameter_is_bad_typed_ipe_raised():
+def test_when_parameter_is_bad_typed_then_ipe_raised():
 
     with pytest.raises(
-        InvalidParamException, match="Invalid parameter value for 'name'"
+        InvalidParamException,
+        match="Invalid parameter value for name. Expecting 'number', but got 'string'.",
     ):
         validate_parameter(
             parameter_value="1", parameter_name="name", parameter_type=int
+        )
+
+
+def test_when_actual_parameter_is_non_json_typed_then_ipe_raised_with_general_error():
+
+    with pytest.raises(
+        InvalidParamException, match="Invalid parameter value for 'name'."
+    ):
+        validate_parameter(
+            parameter_value="1", parameter_name="name", parameter_type=tuple
         )
