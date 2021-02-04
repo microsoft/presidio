f49d11a40f4bb4888e61a8db73bf45d9c50609ab
Shiran Rubin <shiranrubin@gmail.com>
2021-02-04 13:43:52 +0200

    fix code review comments. (#471)
    * fix code review comments.
    
    * fix code review comments.

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -34,20 +34,12 @@ class Server:
             engine = AnonymizerEngine()
             data = AnonymizerRequest(content, engine.builtin_anonymizers)
             text = engine.anonymize(data)
-            return jsonify(text=text)
+            return jsonify(result=text)
 
         @self.app.route("/anonymizers", methods=["GET"])
         def anonymizers() -> Tuple[str, int]:
             """Return a list of supported anonymizers."""
-            try:
-                return json.dumps(AnonymizerEngine().anonymizers()), 200
-            except Exception as e:
-                self.logger.error(
-                    "A fatal error occurred "
-                    "during execution of "
-                    "anonymizers. {}".format(e)
-                )
-                return ErrorResponse(e.args[0]).to_json(), 500
+            return json.dumps(AnonymizerEngine().anonymizers()), 200
 
         @self.app.errorhandler(InvalidParamException)
         def invalid_param(err):
@@ -56,8 +48,11 @@ class Server:
             return ErrorResponse(err.err_msg).to_json(), 422
 
         @self.app.errorhandler(Exception)
-        def server_error(err):
-            self.logger.error(f"failed to anonymize text with error: {err}")
+        def server_error(e):
+            self.logger.error(
+                "A fatal error occurred "
+                "during execution".format(e)
+            )
             return ErrorResponse("Internal server error").to_json(), 500
 
 
