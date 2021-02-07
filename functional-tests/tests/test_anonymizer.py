2923e315d339ccfb8a145d7b7b4743afd63ed521
Nava Vaisman Levy <navalev@users.noreply.github.com>
2021-02-07 11:12:57 +0200

    formatting

--- functional-tests/tests/test_anonymizer.py
+++ functional-tests/tests/test_anonymizer.py
@@ -17,14 +17,17 @@ def test_given_anonymize_called_with_valid_request_then_expected_valid_response_
             { "start": 24, "end": 32, "score": 0.8, "entity_type": "NAME" },
             { "start": 24, "end": 28, "score": 0.8, "entity_type": "FIRST_NAME" },
             { "start": 29, "end": 32, "score": 0.6, "entity_type": "LAST_NAME" },
-            { "start": 48, "end": 57,  "score": 0.95, "entity_type": "PHONE_NUMBER" }
+            { "start": 48, "end": 57,  "score": 0.95,
+                "entity_type": "PHONE_NUMBER" }
         ]
     }
     """
 
     response_status, response_content = anonymize(request_body)
 
-    expected_response = """{"result":"hello world, my name is ANONYMIZED. My number is: 03445****"}"""
+    expected_response = (
+        """{"result":"hello world, my name is ANONYMIZED. My number is: 03445****"}"""
+    )
     assert response_status == 200
     assert equal_json_strings(expected_response, response_content)
 
@@ -47,6 +50,50 @@ def test_given_anonymize_called_with_empty_text_then_invalid_input_message_retur
 
     expected_response = '{"error": "Invalid input, text can not be empty"}'
     assert response_status == 422
+    assert equal_json_strings(expected_response, response_content)
+
+
+pytest.mark.api
+
+
+def test_given_anonymize_called_with_empty_analyzer_results_then_invalid_input_message_returned():
+    request_body = """
+    {
+        "text": "hello world, my name is Jane Doe. My number is: 034453334",
+        "transformations": {
+            "DEFAULT": { "type": "replace", "new_value": "ANONYMIZED" },
+            "PHONE_NUMBER": { "type": "mask", "masking_char": "*", "chars_to_mask": 4, "from_end": true }
+        },
+        "analyzer_results": [
+
+        ]
+    }
+    """
+    response_status, response_content = anonymize(request_body)
+
+    expected_response = '{"error": "Invalid input, analyzer results can not be empty"}'
+    assert response_status == 422
+    assert equal_json_strings(expected_response, response_content)
+
+
+@pytest.mark.api
+def test_given_anonymize_called_with_deformed_body_than_internal_server_error_returned():
+    request_body = """
+    {
+        "text": "hello world, my name is Jane Doe. My number is: 034453334",
+        "transformations": {
+            "DEFAULT": {"type": "replace", "new_value": "ANONYMIZED"},
+        },
+        "analyzer_results": [
+            {"start": 24, "end": 32, "score": 0.8, "entity_type": "NAME"},
+            {"start": 24, "end": 28, "score": 0.8, "entity_type": "FIRST_NAME"},
+        ]
+    }
+    """
+    response_status, response_content = anonymize(request_body)
+
+    expected_response = '{"error": "Internal server error"}'
+    assert response_status == 500
     assert equal_json_strings(expected_response, response_content)
 
 
