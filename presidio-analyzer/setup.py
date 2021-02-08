a8a4aabc8e9e1a8586d5160b283557308cfd38eb
sharon <sharon.hart@microsoft.com>
2021-02-08 10:41:15 +0200

    Add README as package description

--- presidio-analyzer/setup.py
+++ presidio-analyzer/setup.py
@@ -5,9 +5,12 @@ from os import path
 import setuptools
 
 __version__ = ""
-parent_directory = os.path.abspath(
-    os.path.join(path.abspath(path.dirname(__file__)), os.pardir)
-)
+this_directory = path.abspath(path.dirname(__file__))
+parent_directory = os.path.abspath(os.path.join(this_directory, os.pardir))
+
+with open(path.join(this_directory, 'README.MD'), encoding='utf-8') as f:
+    long_description = f.read()
+
 with open(os.path.join(parent_directory, "VERSION")) as version_file:
     __version__ = version_file.read().strip()
 
@@ -36,4 +39,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
