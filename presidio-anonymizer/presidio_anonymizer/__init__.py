7bc60e6305f3e414638363f52ab9c4598943ddc5
Omri Mendels <omri374@users.noreply.github.com>
2021-02-08 14:34:25 +0200

    Configurable logging (#469)

--- presidio-anonymizer/presidio_anonymizer/__init__.py
+++ presidio-anonymizer/presidio_anonymizer/__init__.py
@@ -3,6 +3,7 @@ import logging
 
 from presidio_anonymizer.anonymizer_engine import AnonymizerEngine
 
+# Set up default logging (with NullHandler)
 logging.getLogger("presidio-anonymizer").addHandler(logging.NullHandler())
 
 __all__ = ["AnonymizerEngine"]
