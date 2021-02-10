c0f9586e2915c74938881c53604543de90bae6a3
shiranrubin <shiranrubin@microsoft.com>
2021-02-10 15:39:29 +0200

    add tests and fix bugs.

--- presidio-image-anonymizer/tests/test_image_anonymizer_api.py
+++ presidio-image-anonymizer/tests/test_image_anonymizer_api.py
@@ -23,5 +23,28 @@ def test_given_json_string_then_we_get_json_back(str_json):
 
 
 def test_given_invalid_json_string_then_we_get_an_invalid_param_exception():
-    with pytest.raises(InvalidParamException, match='Invalid json format \'not_json\''):
+    with pytest.raises(InvalidParamException, match="Invalid json format 'not_json'"):
         Server()._get_json_data("not_json")
+
+
+def test_given_empty_json_params_then_we_send_default_color_fill():
+    Server()._color_fill_string_to_value({}) == (0, 0, 0)
+
+
+@pytest.mark.parametrize(
+    # fmt: off
+    "json_params,expected_result",
+    [
+        ({"color_fill": "1"}, 1),
+        ({"color_fill": "1, 0, 1"}, (1, 0, 1))
+    ],
+    # fmt: on
+)
+def test_given_json_params_then_we_extract_properly_color_fill(json_params,
+                                                               expected_result):
+    Server()._color_fill_string_to_value(json_params) == expected_result
+
+
+def test_given_invalid_color_fill_then_get_an_invalid_param_exception():
+    with pytest.raises(InvalidParamException, match="Invalid color fill 'bla'"):
+        Server()._color_fill_string_to_value({"color_fill": "bla"})
