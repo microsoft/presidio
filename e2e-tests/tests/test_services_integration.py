41101f0ad751c1509ed6caa1bbdefde545634694
sharon <sharon.hart@microsoft.com>
2021-02-11 11:20:02 +0200

    build

--- e2e-tests/tests/test_services_integration.py
+++ e2e-tests/tests/test_services_integration.py
@@ -3,7 +3,7 @@ import pytest
 from common.methods import analyzer_supported_entities
 
 
-# TODO: [ADO-2763] Stub, remove and replace with real integration scenarios
+# TODO: [ADO-2763] Stub, remove and replace with real integration scenarios.
 @pytest.mark.integration
 def test_given_a_correct_input_for_supported_entities_then_expect_a_correct_response():
     language_query_parameter = "language=en"
