3aed2eacadd8fadeb4f2bea0a0d7ffe0efe059f4
sharon <sharon.hart@microsoft.com>
2021-02-14 12:15:32 +0200

    Add env file with defaults for compose

--- run.bat
+++ run.bat
@@ -1,8 +1,5 @@
 echo >/dev/null # >nul & GOTO WINDOWS & rem ^
 
-export REGISTRY_NAME=
-export IMAGE_PREFIX=
-export TAG=
 E2E_TESTS_DIR=e2e-tests
 docker-compose up --build -d
 python -m venv ${E2E_TESTS_DIR}/env
@@ -15,9 +12,6 @@ exit 0
 :WINDOWS
 
 @echo off
-set REGISTRY_NAME=
-set IMAGE_PREFIX=
-set TAG=
 set E2E_TESTS_DIR=e2e-tests
 docker-compose up --build -d
 if not exist "%E2E_TESTS_DIR%\env" py -m venv %E2E_TESTS_DIR%\env
