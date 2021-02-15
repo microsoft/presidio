d9449b613363dc6b9b71257d56a57fd47fc8df76
shiranrubin <shiranrubin@microsoft.com>
2021-02-15 10:17:22 +0200

    fix PR comments

--- presidio-image-anonymizer/tests/integration/test_image_anonymizer_engine.py
+++ presidio-image-anonymizer/tests/integration/test_image_anonymizer_engine.py
@@ -1,6 +1,8 @@
 from methods import get_resource_image, compare_images
 from presidio_image_anonymizer import ImageAnonymizerEngine
 
+red_fill = (255, 0, 0)
+
 
 def test_given_image_with_text_and_fill_then_text_is_greyed_out():
     # Image with PII entities
@@ -13,8 +15,9 @@ def test_given_image_with_text_and_fill_then_text_is_greyed_out():
 def test_given_image_with_text_and_matrix_fill_then_text_is_colored_out():
     # Image with PII entities
     image = get_resource_image("ocr_test.png")
+
+    anonymized_image = ImageAnonymizerEngine().anonymize(image, red_fill)
     expected_result_image = get_resource_image("ocr_test_anonymized_matrix.png")
-    anonymized_image = ImageAnonymizerEngine().anonymize(image, (255, 160, 122))
     assert compare_images(anonymized_image, expected_result_image)
     assert not compare_images(anonymized_image, image)
 
@@ -22,5 +25,5 @@ def test_given_image_with_text_and_matrix_fill_then_text_is_colored_out():
 def test_given_image_without_text_and_fill_then_image_does_not_change():
     # Image without PII entities
     image = get_resource_image("no_ocr.jpg")
-    anonymized_image = ImageAnonymizerEngine().anonymize(image, (255, 160, 122))
+    anonymized_image = ImageAnonymizerEngine().anonymize(image, red_fill)
     assert compare_images(anonymized_image, image)
