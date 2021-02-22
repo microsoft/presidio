5f52985a03da0c2918c7c47181656dcb3a2c9687
shiranrubin <shiranrubin@microsoft.com>
2021-02-22 14:09:01 +0200

    Add E2E tests

--- e2e-tests/common/methods.py
+++ e2e-tests/common/methods.py
@@ -1,5 +1,4 @@
 import os
-from typing import Dict
 
 import requests
 
@@ -42,16 +41,28 @@ def analyzer_supported_entities(data):
     return response.status_code, response.content
 
 
-def image_redactor(file_name: str, color_fill):
-    current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, "..", "resources", file_name)
-    multipart_form_data = {
-        "image": (file_name, open(file_path, "rb"), "multipart/form-data"),
-    }
-    payload = {}
-    if color_fill:
-        payload = {"data": "{'color_fill':'" + str(color_fill) + "'}"}
+def image_redactor(file_name: str, color_fill=None):
+    multipart_form_data = get_multipart_form_data(file_name)
+    payload = get_payload(color_fill)
     response = requests.post(f"{IMAGE_REDACTOR_BASE_URL}/redact",
                              files=multipart_form_data, data=payload
                              )
-    return response.status_code, response.content
+    return response
+
+
+def get_payload(color_fill):
+    payload = {}
+    if color_fill:
+        payload = {"data": "{'color_fill':'" + str(color_fill) + "'}"}
+    return payload
+
+
+def get_multipart_form_data(file_name):
+    multipart_form_data = {}
+    if file_name:
+        current_dir = os.path.dirname(__file__)
+        file_path = os.path.join(current_dir, "..", "resources", file_name)
+        multipart_form_data = {
+            "image": (file_name, open(file_path, "rb"), "multipart/form-data"),
+        }
+    return multipart_form_data
--- e2e-tests/tests/test_image_redactor.py
+++ e2e-tests/tests/test_image_redactor.py
@@ -4,34 +4,41 @@ from common.methods import image_redactor
 
 
 def test_given_image_with_int_color_filling_then_we_get_redact_image():
-    # black redact
-    expected_result_image = get_file("black_redact.png")
-    status, result_image = image_redactor("original_image.png", 0)
-    assert result_image == expected_result_image.read()
-    assert status == 200
+    # gray redact
+    expected_result_image = get_file("gray_redact.png")
+    response = image_redactor("original_image.png", 255)
+    validate_and_close(expected_result_image, response)
 
 
 def test_given_image_with_tuple_color_filling_then_we_get_redact_image():
     # purple redact
     expected_result_image = get_file("purple_redact.png")
-    status, result_image = image_redactor("original_image.png", (102, 0, 102))
-    assert result_image == expected_result_image.read()
-    assert status == 200
+    response = image_redactor("original_image.png", "102, 0, 102")
+    validate_and_close(expected_result_image, response)
 
 
 def test_given_image_with_no_color_filling_then_we_get_redact_image():
-    # purple redact
-    expected_result_image = get_file("purple_redact.png")
-    status, result_image = image_redactor("original_image.png")
-    assert result_image == expected_result_image.read()
-    assert status == 200
+    # black redact
+    expected_result_image = get_file("black_redact.png")
+    response = image_redactor("original_image.png")
+    validate_and_close(expected_result_image, response)
 
 
-def compare_images(image_one, image_two):
-    return image_one == image_two.read()
+def test_given_no_image_then_we_fail():
+    # black redact
+    expected_response = '{"error": "Invalid parameter, please insert image data"}'
+    response = image_redactor("")
+    assert response.status_code == 422
+    assert response.content.decode() == expected_response
 
 
 def get_file(file_name: str):
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, "..", "resources", file_name)
     return open(file_path, "rb")
+
+
+def validate_and_close(expected_result_image, response):
+    assert response.status_code == 200
+    assert response.content == expected_result_image.read()
+    expected_result_image.close()
