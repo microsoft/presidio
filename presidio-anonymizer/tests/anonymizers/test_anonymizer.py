cba42acf38802701de611b90df427070aa20f100
shiranrubin <shiranrubin@microsoft.com>
2021-02-24 10:43:35 +0200

    Remove FPE since we currently do not support it.

--- presidio-anonymizer/tests/anonymizers/test_anonymizer.py
+++ presidio-anonymizer/tests/anonymizers/test_anonymizer.py
@@ -4,5 +4,5 @@ from presidio_anonymizer.anonymizers import Anonymizer
 def test_given_anonymizers_list_then_all_classes_are_there():
     anonymizers = Anonymizer.get_anonymizers()
     assert len(anonymizers) >= 5
-    for class_name in ["hash", "mask", "redact", "replace", "fpe"]:
+    for class_name in ["hash", "mask", "redact", "replace"]:
         assert anonymizers.get(class_name)
