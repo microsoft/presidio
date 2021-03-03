e140203b0991e5056af2ad2de8094ebc1b7a33b7
omri374 <omri.mendels@microsoft.com>
2021-03-03 11:25:23 +0200

    align analyzer and anonymizer RecognizerResult objects

--- presidio-analyzer/presidio_analyzer/recognizer_result.py
+++ presidio-analyzer/presidio_analyzer/recognizer_result.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict
 
 from presidio_analyzer import AnalysisExplanation
@@ -16,6 +17,8 @@ class RecognizerResult:
     :param analysis_explanation: contains the explanation of why this
                                  entity was identified
     """
+
+    logger = logging.getLogger("presidio-analyzer")
 
     def __init__(
         self,
@@ -45,14 +48,24 @@ class RecognizerResult:
         """
         return self.__dict__
 
-    def __str__(self) -> str:
-        """Return a string representation of the instance."""
-        return (
-            f"type: {self.entity_type}, "
-            f"start: {self.start}, "
-            f"end: {self.end}, "
-            f"score: {self.score}"
-        )
+    @classmethod
+    def from_json(cls, data: Dict):
+        """
+        Create RecognizerResult from json.
+
+        :param data: e.g. {
+            "start": 24,
+            "end": 32,
+            "score": 0.8,
+            "entity_type": "NAME"
+        }
+        :return: RecognizerResult
+        """
+        score = data.get("score")
+        entity_type = data.get("entity_type")
+        start = data.get("start")
+        end = data.get("end")
+        return cls(entity_type, start, end, score)
 
     def __repr__(self) -> str:
         """Return a string representation of the instance."""
@@ -80,3 +93,77 @@ class RecognizerResult:
         :return: true if contained
         """
         return self.start >= other.start and self.end <= other.end
+
+    def contains(self, other):
+        """
+        Check if one result is contained or equal to another result.
+
+        :param other: another RecognizerResult
+        :return: bool
+        """
+        return self.start <= other.start and self.end >= other.end
+
+    def equal_indices(self, other):
+        """
+        Check if the indices are equal between two results.
+
+        :param other: another RecognizerResult
+        :return:
+        """
+        return self.start == other.start and self.end == other.end
+
+    def __gt__(self, other):
+        """
+        Check if one result is greater by using the results indices in the text.
+
+        :param other: another RecognizerResult
+        :return: bool
+        """
+        if self.start == other.start:
+            return self.end > other.end
+        return self.start > other.start
+
+    def __eq__(self, other):
+        """
+        Check two results are equal by using all class fields.
+
+        :param other: another RecognizerResult
+        :return: bool
+        """
+        equal_type = self.entity_type == other.entity_type
+        equal_score = self.score is other.score
+        return self.equal_indices(other) and equal_type and equal_score
+
+    def __hash__(self):
+        """
+        Hash the result data by using all class fields.
+
+        :return: int
+        """
+        return hash(
+            f"{str(self.start)} {str(self.end)} {str(self.score)} {self.entity_type}"
+        )
+
+    def __str__(self) -> str:
+        """Return a string representation of the instance."""
+        return (
+            f"type: {self.entity_type}, "
+            f"start: {self.start}, "
+            f"end: {self.end}, "
+            f"score: {self.score}"
+        )
+
+    def has_conflict(self, other):
+        """
+        Check if two recognizer results are conflicted or not.
+
+        I have a conflict if:
+        1. My indices are the same as the other and my score is lower.
+        2. If my indices are contained in another.
+
+        :param other: RecognizerResult
+        :return:
+        """
+        if self.equal_indices(other):
+            return self.score <= other.score
+        return other.contains(self)
