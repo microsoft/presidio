f019ca064c1beef73b22455a6c75191dafad5beb
sharon <sharon.hart@microsoft.com>
2021-03-04 16:39:23 +0200

    Anonymizer - Add encrypt anonymizer

---
+++ presidio-anonymizer/tests/anonymizers/test_encrypt.py
@@ -0,0 +1,31 @@
+from unittest import mock
+
+import pytest
+
+from presidio_anonymizer.anonymizers.encrypt import Encrypt
+from presidio_anonymizer.entities import InvalidParamException
+from presidio_anonymizer.services.aes_cipher import AESCipher
+
+
+@mock.patch.object(AESCipher, "encrypt")
+def test_when_anonymize_then_aes_encrypt_called_and_its_result_is_returned(
+    mock_encrypt,
+):
+    expected_anonymized_text = "encrypted_text"
+    mock_encrypt.return_value = expected_anonymized_text
+
+    anonymized_text = Encrypt().anonymize(text="text", params={"key": "key"})
+
+    assert anonymized_text == expected_anonymized_text
+
+
+def test_when_verifying_an_valid_length_key_no_exceptions_raised():
+    Encrypt().validate(params={"key": "128bitslengthkey"})
+
+
+def test_when_verifying_an_invalid_length_key_then_ipe_raised():
+    with pytest.raises(
+        InvalidParamException,
+        match="Invalid input, key must of length 128, 192 or 256 bits",
+    ):
+        Encrypt().validate(params={"key": "key"})
