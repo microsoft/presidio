cb3a8cddd95d0bb44648d837c8cf01facb3f2699
sharon <sharon.hart@microsoft.com>
2021-03-08 10:57:54 +0200

    change test names

--- presidio-anonymizer/tests/anonymizers/test_encrypt.py
+++ presidio-anonymizer/tests/anonymizers/test_encrypt.py
@@ -8,7 +8,7 @@ from presidio_anonymizer.services.aes_cipher import AESCipher
 
 
 @mock.patch.object(AESCipher, "encrypt")
-def test_when_anonymize_then_aes_encrypt_called_and_its_result_is_returned(
+def test_given_anonymize_then_aes_encrypt_called_and_its_result_is_returned(
     mock_encrypt,
 ):
     expected_anonymized_text = "encrypted_text"
@@ -19,11 +19,11 @@ def test_when_anonymize_then_aes_encrypt_called_and_its_result_is_returned(
     assert anonymized_text == expected_anonymized_text
 
 
-def test_when_verifying_an_valid_length_key_no_exceptions_raised():
+def test_given_verifying_an_valid_length_key_no_exceptions_raised():
     Encrypt().validate(params={"key": "128bitslengthkey"})
 
 
-def test_when_verifying_an_invalid_length_key_then_ipe_raised():
+def test_given_verifying_an_invalid_length_key_then_ipe_raised():
     with pytest.raises(
         InvalidParamException,
         match="Invalid input, key must of length 128, 192 or 256 bits",
