f3446c6d83b7bacf02b74338e5ae8fd42745d494
Itye <itye@ityer-2.local>
2021-03-10 17:21:12 +0200

    more tests

--- presidio-anonymizer/tests/test_anonymizer_result.py
+++ presidio-anonymizer/tests/test_anonymizer_result.py
@@ -23,3 +23,34 @@ def test_when_normalized_items_called_then_idexes_are_normalized():
     res.normalize_item_indexes()
     assert res.items[0].start == 3
     assert res.items[0].end == 5
+
+
+def test_when_set_text_then_text_is_set():
+    res = AnonymizerResult()
+    res.set_text("a")
+    assert res.text == "a"
+
+
+def test_when_add_item_the_item_added():
+    res = AnonymizerResult()
+    ari = AnonymizedTextIndexItem("a", "b", 1, 2, "cd")
+    res.add_item(ari)
+    assert res.items[0] == ari
+
+
+def test_when_eq_called_then_instances_are_equal():
+    res = AnonymizerResult()
+    res.set_text("a")
+    res2 = AnonymizerResult()
+    res2.set_text("a")
+
+    assert res.__eq__(res2)
+
+
+def test_when_not_eq_called_then_instances_are_not_equal():
+    res = AnonymizerResult()
+    res.set_text("a")
+    res2 = AnonymizerResult()
+    res2.set_text("b")
+
+    assert res.__eq__(res2) is False
