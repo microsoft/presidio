ad8cd8317fbb55fd08a6c4ff1f63fce8f46b60d8
sharon <sharon.hart@microsoft.com>
2021-03-25 15:08:12 +0200

    e2e on 3.9

--- presidio-anonymizer/setup.py
+++ presidio-anonymizer/setup.py
@@ -30,9 +30,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     description="Persidio Anonymizer package - replaces analyzed text with desired "
     "values.",
