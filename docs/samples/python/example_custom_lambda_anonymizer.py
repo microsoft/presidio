69fe978dda6a3730ed56ea54e297773ddce6cfd2
Nava Vaisman Levy <navalev@users.noreply.github.com>
2021-04-06 22:18:18 +0300

    fix sample

--- docs/samples/python/example_custom_lambda_anonymizer.py
+++ docs/samples/python/example_custom_lambda_anonymizer.py
@@ -1,34 +1,26 @@
 from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
-from presidio_anonymizer.entities import AnonymizerConfig
+from presidio_anonymizer.entities.engine.operator_config import OperatorConfig
 from faker import Faker
 from faker.providers import internet
 
 def reverse_string(x):
   return x[::-1]
 
-def anonymize_reverse_lambda(text_to_anonymize): 
-    analyzer_results = analyzer.analyze(text=text_to_anonymize, entities=["EMAIL_ADDRESS"], language='en')
-
-    print(analyzer_results)    
-
+def anonymize_reverse_lambda(analyzer_results, text_to_anonymize):         
     anonymized_results = anonymizer.anonymize(
         text=text_to_anonymize,
         analyzer_results=analyzer_results,            
-        anonymizers_config={"EMAIL_ADDRESS": AnonymizerConfig("custom", {"new_value": lambda x: reverse_string(x)})}        
+        operators={"EMAIL_ADDRESS": OperatorConfig("custom", {"new_value": lambda x: x[::-1]})}        
     )
 
     return anonymized_results
 
-def anonymize_faker_lambda(text_to_anonymize): 
-    analyzer_results = analyzer.analyze(text=text_to_anonymize, entities=["EMAIL_ADDRESS"], language='en')
-
-    print(analyzer_results)    
-
+def anonymize_faker_lambda(analyzer_results, text_to_anonymize):      
     anonymized_results = anonymizer.anonymize(
         text=text_to_anonymize,
         analyzer_results=analyzer_results,    
-        anonymizers_config={"EMAIL_ADDRESS": AnonymizerConfig("custom", {"new_value": lambda x: fake.safe_email()})}
+        operators={"EMAIL_ADDRESS": OperatorConfig("custom", {"new_value": lambda x: fake.safe_email()})}
     )
 
     return anonymized_results
@@ -40,5 +32,7 @@ analyzer = AnalyzerEngine()
 anonymizer = AnonymizerEngine()
 
 text = 'The user has the following two emails: email1@gmail.com and email2@gmail.com'
-print("Reverse lambda result: " + anonymize_reverse_lambda(text).text + "\n")
-print("Faker lambda result" + anonymize_faker_lambda(text).text + "\n")
+analyzer_results = analyzer.analyze(text=text, entities=["EMAIL_ADDRESS"], language='en')
+
+print("Reverse lambda result: " + anonymize_reverse_lambda(analyzer_results, text).text + "\n")
+print("Faker lambda result: " + anonymize_faker_lambda(analyzer_results, text).text + "\n")
