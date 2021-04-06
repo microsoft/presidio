8b779047eceb901e2a738d5cf36de800ae51eb88
Nava Vaisman Levy <navalev@users.noreply.github.com>
2021-04-06 21:45:23 +0300

    custom used only for fucntions

--- presidio-anonymizer/presidio_anonymizer/operators/custom.py
+++ presidio-anonymizer/presidio_anonymizer/operators/custom.py
@@ -1,4 +1,4 @@
-"""Replaces the PII text with lambda result."""
+"""Replaces the PII text with function result."""
 from typing import Dict
 
 from presidio_anonymizer.operators import Operator, OperatorType
@@ -8,32 +8,28 @@ from presidio_anonymizer.entities import InvalidParamException
 
 class Custom(Operator):
     """
-    Replace PII text entity with the lambda result executed on the PII text.
+    Replace PII text entity with the results of a function executed on the PII text.
 
-    lambda retrun type must be a string
-    If the new value is not a lambda, this will act as a replace method
+    The function retrun type must be a string    
     """
 
     NEW_VALUE = "new_value"
 
     def operate(self, text: str = None, params: Dict = None) -> str:
-        """:return: new_value."""
-        new_val = params.get(self.NEW_VALUE)
-        if not new_val:
-            return f"<{params.get('entity_type')}>"
-        return new_val(text) if callable(new_val) else new_val
+        """:return: result of function executed on the text."""
+        new_val = params.get(self.NEW_VALUE)         
+        return new_val(text)
 
     def validate(self, params: Dict = None) -> None:
-        """Validate the new value is string."""
+        """Validate the provided function is returning a string."""
         new_val = params.get(self.NEW_VALUE)
         if callable(new_val):
-            if (type(new_val("PII")) == str):
-                return
+            if (type(new_val("PII")) == str):            
+                return            
             else:
-                raise InvalidParamException("Invalid method return type. must be a str")
+                raise InvalidParamException("Invalid function return type. must be a str")        
         else:
-            validate_type(new_val, self.NEW_VALUE, str)
-        pass
+            raise InvalidParamException("New value must be a callable function")        
 
     def operator_name(self) -> str:
         """Return operator name."""
--- presidio-anonymizer/tests/operators/test_custom.py
+++ presidio-anonymizer/tests/operators/test_custom.py
@@ -4,9 +4,12 @@ from presidio_anonymizer.operators import Custom
 from presidio_anonymizer.entities import InvalidParamException
 
 
-def test_given_value_for_custom_then_we_get_the_value_back():
-    text = Custom().operate("", {"new_value": "bla"})
-    assert text == "bla"
+def test_given_non_callable_for_custom_then_ipe_raised():
+    with pytest.raises(
+        InvalidParamException,
+        match="New value must be a callable function",
+    ):
+        Custom().validate({"new_value": "bla"})    
 
 
 def test_given_lambda_for_custom_we_get_the_result_back():
@@ -17,7 +20,7 @@ def test_given_lambda_for_custom_we_get_the_result_back():
 def test_given_non_str_lambda_than_ipe_raised():
     with pytest.raises(
         InvalidParamException,
-        match="Invalid method return type. must be a str",
+        match="Invalid function return type. must be a str",
     ):
         Custom().validate({"new_value" : lambda x: len(x)})
 
