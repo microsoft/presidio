7448095631c25f72e1b39d8181edebf5b309b382
abner <abner.zhangxinbin.anz@gmail.com>
2021-04-09 08:49:40 +1000

    add matplotlib to setup.py and Pipfile

--- presidio-image-redactor/setup.py
+++ presidio-image-redactor/setup.py
@@ -4,7 +4,12 @@ from os import path
 
 from setuptools import setup, find_packages
 
-requirements = ["pillow", "pytesseract==0.3.7", "presidio-analyzer>=1.9.0"]
+requirements = [
+    "pillow",
+    "matplotlib",
+    "pytesseract==0.3.7",
+    "presidio-analyzer>=1.9.0",
+]
 
 test_requirements = ["pytest>=3", "flake8==3.7.9"]
 
