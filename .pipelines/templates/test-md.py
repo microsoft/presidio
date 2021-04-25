94bb5f04e8cec946a706c6bec4ac4f7cd7cf7333
Nava Vaisman Levy <navalev@users.noreply.github.com>
2021-04-25 16:48:07 +0300

    initial pipeline

---
+++ .pipelines/templates/test-md.py
@@ -0,0 +1,16 @@
+import os
+import exdown
+
+
+for root, dirs, files in os.walk("."):
+  for file in files:                
+      if (file.endswith(".md") or file.endswith(".MD")):  
+        filename = os.path.join(root,file)           
+        print(filename)             
+        blocks = exdown.extract(filename, encoding=None, syntax_filter="python")                             
+        for code, line in blocks:                      
+          try:
+            exec(code)
+          except Exception:
+            print(f"{filename} (line {line}) failed")   
+            raise
