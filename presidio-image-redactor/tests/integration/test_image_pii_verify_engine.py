4dd5c66faed7877dd747822558c3a63d7d2cdc9c
zhangabner <abner.zhangxinbin.anz@gmail.com>
2021-04-27 09:03:13 +1000

    Update test_image_pii_verify_engine.py

--- presidio-image-redactor/tests/integration/test_image_pii_verify_engine.py
+++ presidio-image-redactor/tests/integration/test_image_pii_verify_engine.py
@@ -7,12 +7,3 @@ def test_given_image_without_text_and_pii_verify_then_image_does_not_change():
     image = get_resource_image("no_ocr.png")
     pii_verifying_image = ImagePiiVerifyEngine().verify(image)
     assert compare_images(pii_verifying_image, image)
-
-
-def test_given_image_with_text_and_pii_is_shown():
-    # Image with PII entities
-    image = get_resource_image("ocr_test.png")
-    pii_verifying_image = ImagePiiVerifyEngine().verify(image)
-    expected_result_image = get_resource_image("pii_verify.png")
-    assert compare_images(pii_verifying_image, expected_result_image)
-    assert not compare_images(pii_verifying_image, image)
