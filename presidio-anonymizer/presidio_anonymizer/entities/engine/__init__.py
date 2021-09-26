31e8675930409d70b628291ead89d49ed4787951
Omri Mendels <omri374@users.noreply.github.com>
2021-09-26 14:49:48 +0300

    Changes to Anonymizer packages/imports + added repr to classes. (#763)
    * modules changes, doc changes
    
    * updates on notebook and e2e
    
    * backward compatibility of OperatorConfig
    
    * revert file structure
    
    * revert customize_presidio_analyzer, doesn't belong in this PR
    
    * update to spark notebook
    
    * updates to notebook
    
    * Update getting_entity_values.ipynb
    
    * Update getting_entity_values.ipynb
    
    * grouped imports
    
    * Update test_context_support.py

--- presidio-anonymizer/presidio_anonymizer/entities/engine/__init__.py
+++ presidio-anonymizer/presidio_anonymizer/entities/engine/__init__.py
@@ -4,9 +4,4 @@ from .operator_config import OperatorConfig
 from .recognizer_result import RecognizerResult
 from .anonymizer_result import AnonymizerResult
 
-__all__ = [
-    "PIIEntity",
-    "OperatorConfig",
-    "RecognizerResult",
-    "AnonymizerResult"
-]
+__all__ = ["PIIEntity", "OperatorConfig", "RecognizerResult", "AnonymizerResult"]
--- presidio-anonymizer/presidio_anonymizer/entities/engine/pii_entity.py
+++ presidio-anonymizer/presidio_anonymizer/entities/engine/pii_entity.py
@@ -2,8 +2,11 @@ import logging
 from abc import ABC
 
 from presidio_anonymizer.entities import InvalidParamException
-from presidio_anonymizer.services.validators import validate_parameter_not_empty, \
-    validate_parameter_exists, validate_type
+from presidio_anonymizer.services.validators import (
+    validate_parameter_not_empty,
+    validate_parameter_exists,
+    validate_type,
+)
 
 
 class PIIEntity(ABC):
@@ -16,15 +19,25 @@ class PIIEntity(ABC):
         self.entity_type = entity_type
         self.__validate_fields()
 
+    def __repr__(self):
+        """Return a string representation of the object."""
+        return (
+            f"start: {self.start}"
+            f"end: {self.end},"
+            f"entity_type: {self.entity_type}"
+        )
+
     def __gt__(self, other):
         """Check one entity is greater then other by the text end index."""
         return self.start > other.start
 
     def __eq__(self, other):
         """Check two text metadata entities are equal."""
-        return (self.start == other.start
-                and self.end == other.end
-                and self.entity_type == other.entity_type)
+        return (
+            self.start == other.start
+            and self.end == other.end
+            and self.entity_type == other.entity_type
+        )
 
     def __validate_fields(self):
         validate_parameter_exists(self.start, "result", "start")
