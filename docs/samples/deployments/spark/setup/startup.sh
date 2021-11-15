6f1582b591ad66a35d9a3a502d22a5e2ce217466
sharon <sharon.hart@microsoft.com>
2021-11-15 04:50:40 +0200

    restore sync

--- docs/samples/deployments/spark/setup/startup.sh
+++ docs/samples/deployments/spark/setup/startup.sh
@@ -3,4 +3,4 @@
 pip install presidio-analyzer
 pip install presidio-anonymizer
 pip install azure-storage-blob
-pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-3.2.0/en_core_web_lg-3.2.0-py3-none-any.whl
+python -m spacy download en_core_web_lg
