dcce922eefae8063c83743ddc32cbf844eabc11c
omri374 <omri.mendels@microsoft.com>
2022-01-16 15:39:07 +0200

    re-locked image redactor to support new PIL + removed 3.6 from build (EOL)

--- presidio-analyzer/setup.py
+++ presidio-analyzer/setup.py
@@ -42,10 +42,10 @@ setuptools.setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
