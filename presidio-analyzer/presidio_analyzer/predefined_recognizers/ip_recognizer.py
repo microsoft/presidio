a9d885ddc16a162c65cf98e2526b8b92e4619e85
sharon <sharon.hart@microsoft.com>
2022-12-04 14:36:15 +0200

    Add :: case and invalidation

--- presidio-analyzer/presidio_analyzer/predefined_recognizers/ip_recognizer.py
+++ presidio-analyzer/presidio_analyzer/predefined_recognizers/ip_recognizer.py
@@ -1,3 +1,4 @@
+import ipaddress
 from typing import Optional, List
 
 from presidio_analyzer import Pattern, PatternRecognizer
@@ -24,6 +25,11 @@ class IpRecognizer(PatternRecognizer):
             r"\b(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\b",  # noqa: E501
             0.6,
         ),
+        Pattern(
+            "IPv6",
+            r"::",
+            0.1,
+        ),
     ]
 
     CONTEXT = ["ip", "ipv4", "ipv6"]
@@ -43,3 +49,14 @@ class IpRecognizer(PatternRecognizer):
             context=context,
             supported_language=supported_language,
         )
+
+    def invalidate_result(self, pattern_text: str) -> bool:
+        """
+        Check if the pattern text cannot be validated as an IP address.
+        :param pattern_text: Text detected as pattern by regex
+        :return: True if invalidated
+        """
+        try:
+            ipaddress.ip_address(pattern_text)
+        except ValueError:
+            return True
