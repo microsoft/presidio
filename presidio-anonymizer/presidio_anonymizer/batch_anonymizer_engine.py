0e4c2292557130c5a4f9e0d55206437fbb0a7f49
Nava Vaisman Levy <navalev@users.noreply.github.com>
2023-01-01 15:51:16 +0200

    batch anonymizer

---
+++ presidio-anonymizer/presidio_anonymizer/batch_anonymizer_engine.py
@@ -0,0 +1,71 @@
+from typing import List, Dict, Union, Iterable
+import collections
+
+from presidio_analyzer import RecognizerResult, DictAnalyzerResult
+from presidio_anonymizer import AnonymizerEngine
+from presidio_anonymizer.entities import EngineResult
+
+
+class BatchAnonymizerEngine(AnonymizerEngine):
+    """
+    BatchAnonymizerEngine class.
+
+    Class inheriting from the AnonymizerEngine, with additional functionality
+    for anonymizing lists or dictionaries.
+    """
+
+    def anonymize_list(
+        self,
+        texts: List[Union[str, bool, int, float]],
+        recognizer_results_list: List[List[RecognizerResult]],
+        **kwargs
+    ) -> List[EngineResult]:
+        """
+        Anonymize a list of strings.
+
+        :param texts: List containing the texts to be anonymized (original texts)
+        :param recognizer_results_list: A list of lists of RecognizerResult,
+        the output of the AnalyzerEngine on each text in the list.
+        :param kwargs: Additional kwargs for the `AnonymizerEngine.anonymize` method
+        """
+        return_list = []
+        if not recognizer_results_list:
+            recognizer_results_list = [[] for _ in range(len(texts))]
+        for text, recognizer_results in zip(texts, recognizer_results_list):
+            if type(text) in (str, bool, int, float):
+                res = self.anonymize(text=str(text), analyzer_results=recognizer_results, **kwargs)
+                return_list.append(res.text)
+            else:
+                return_list.append(text)
+
+        return return_list
+
+    def anonymize_dict(self, analyzer_results: Iterable[DictAnalyzerResult], **kwargs) -> Dict[str, str]:
+
+        """
+        Anonymize values in a dictionary.
+
+        :param analyzer_results: Iterator of `DictAnalyzerResult` 
+        containing the output of the AnalyzerEngine.analyze_dict on the input text.
+        :param kwargs: Additional kwargs for the `AnonymizerEngine.anonymize` method
+        """
+
+        return_dict = {}
+        for result in analyzer_results:
+
+            if isinstance(result.value, dict):
+                resp = self.anonymize_dict(analyzer_results=result.recognizer_results, **kwargs)
+                return_dict[result.key] = resp
+
+            elif isinstance(result.value, str):
+                resp = self.anonymize(text=result.value, analyzer_results=result.recognizer_results, **kwargs)
+                return_dict[result.key] = resp.text
+
+            elif isinstance(result.value, collections.abc.Iterable):
+                anonymize_respones = self.anonymize_list(texts=result.value,
+                                                         recognizer_results_list=result.recognizer_results,
+                                                         **kwargs)
+                return_dict[result.key] = anonymize_respones
+            else:
+                return_dict[result.key] = result.value
+        return return_dict
---
+++ presidio-anonymizer/tests/test_batch_anonymizer_engine.py
@@ -0,0 +1,59 @@
+from presidio_anonymizer import BatchAnonymizerEngine
+from presidio_analyzer import RecognizerResult, DictAnalyzerResult
+from presidio_anonymizer.entities import OperatorConfig
+
+
+texts = ['John', 'Jill', 'Jack']
+recognizer_results_list = [[RecognizerResult('PERSON', 0, 4, 0.85)],
+                           [RecognizerResult('PERSON', 0, 4, 0.85)],
+                           [RecognizerResult('PERSON', 0, 4, 0.85)]]
+analyzer_results = [DictAnalyzerResult(key='name',
+                                       value=texts,
+                                       recognizer_results=recognizer_results_list)]
+
+
+def test_given_analyzer_result_we_anonymize_dict_correctly():
+    engine = BatchAnonymizerEngine()
+    anonymized_results = engine.anonymize_dict(analyzer_results)
+    assert anonymized_results == {'name': ['<PERSON>', '<PERSON>', '<PERSON>']}
+
+
+def test_given_analyzer_result_we_anonymize_list_correctly():
+    engine = BatchAnonymizerEngine()
+    anonymized_results = engine.anonymize_list(texts=texts, recognizer_results_list=recognizer_results_list)
+    assert anonymized_results == ['<PERSON>', '<PERSON>', '<PERSON>']
+
+
+def test_given_empty_recognizers_than_we_return_text_unchanged():
+    empty_analyzer_results = [DictAnalyzerResult(key='name',
+                                                 value=texts,
+                                                 recognizer_results=[])]
+    engine = BatchAnonymizerEngine()
+    anonymized_results = engine.anonymize_dict(empty_analyzer_results)
+    assert anonymized_results == {'name': ['John', 'Jill', 'Jack']}
+
+
+def test_given_complex_analyzer_result_we_anonymize_dict_correctly():
+    analyzer_results = [DictAnalyzerResult(key='name',
+                                           value=texts,
+                                           recognizer_results=recognizer_results_list),
+                        DictAnalyzerResult(key='comments',
+                                           value=['called him yesterday to confirm he requested to call back in 2 days', 'accepted the offer license number AC432223', 'need to call him at phone number 212-555-5555'],
+                                           recognizer_results=[[RecognizerResult('DATE_TIME', 11, 20, 0.85), RecognizerResult('DATE_TIME', 61, 67, 0.85)],
+                                                               [RecognizerResult('US_DRIVER_LICENSE', 34, 42, 0.6499999999999999)],
+                                                               [RecognizerResult('PHONE_NUMBER', 33, 45, 0.75)]])]
+    engine = BatchAnonymizerEngine()
+    anonymized_results = engine.anonymize_dict(analyzer_results)
+    assert anonymized_results == {'name': ['<PERSON>', '<PERSON>', '<PERSON>'],
+                                  'comments': ['called him <DATE_TIME> to confirm he requested to call back in '
+                                               '<DATE_TIME>',
+                                               'accepted the offer license number <US_DRIVER_LICENSE>',
+                                               'need to call him at phone number <PHONE_NUMBER>']}
+
+
+def test_given_custom_anonymizer_we_anonymize_dict_correctly():
+    anonymizer_config = OperatorConfig("custom", {"lambda": lambda x: f"<ENTITY: {x}>"})
+
+    engine = BatchAnonymizerEngine()
+    anonymized_results = engine.anonymize_dict(analyzer_results, operators={"DEFAULT": anonymizer_config})
+    assert anonymized_results == {'name': ['<ENTITY: John>', '<ENTITY: Jill>', '<ENTITY: Jack>']}
