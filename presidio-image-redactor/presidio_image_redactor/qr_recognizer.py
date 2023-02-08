611251302673d6fb8156d0695635df641d8f2a6f
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-02-08 17:14:31 +0300

    Move

--- presidio-image-redactor/presidio_image_redactor/qr_recognizer.py
+++ presidio-image-redactor/presidio_image_redactor/qr_recognizer.py
@@ -51,6 +51,7 @@ class OpenCVQRRecongnizer(QRRecognizer):
         if ret:
             text = self._decode(image, points)
 
+            points = points[0].astype(int)
             (x, y, w, h) = cv2.boundingRect(points)
 
             recognized.append(QRRecognizerResult(
@@ -63,7 +64,6 @@ class OpenCVQRRecongnizer(QRRecognizer):
 
     def _detect(self, image) -> Tuple[float, List[Tuple[int, int]]]:
         ret, points = self.detector.detect(image)
-        points = points[0].astype(int)
 
         return ret, points
 
@@ -71,3 +71,27 @@ class OpenCVQRRecongnizer(QRRecognizer):
         res = self.detector.decode(image, points)
 
         return res[0]
+
+
+import numpy as np
+from PIL import Image, ImageDraw
+
+if __name__ == "__main__":
+    image = cv2.imread("qr.jpg")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    rec = OpenCVQRRecongnizer()
+    recognized = rec.recognize(image)
+    #
+    image = Image.fromarray(image)
+    img1 = ImageDraw.Draw(image) 
+
+    for r in recognized:
+        x0 = r.bbox[0]
+        y0 = r.bbox[1]
+        x1 = x0 + r.bbox[2]
+        y1 = y0 + r.bbox[3]
+    
+        img1.rectangle([x0, y0, x1, y1], fill ="#eeeeff", outline ="blue")
+    
+    image.show()
