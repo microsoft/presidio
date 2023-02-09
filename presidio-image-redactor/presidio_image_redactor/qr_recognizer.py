3977ebb4097f9d36dc8d80e4342f36faac404e40
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-02-09 18:30:59 +0300

    Fix multiple qr codes detection and decoding

--- presidio-image-redactor/presidio_image_redactor/qr_recognizer.py
+++ presidio-image-redactor/presidio_image_redactor/qr_recognizer.py
@@ -41,21 +41,36 @@ class QRRecognizer(ABC):
 
 
 class OpenCVQRRecongnizer(QRRecognizer):
+    """
+    QR code recognition using OpenCV.
+
+    Example of  usage:
+
+    .. code-block:: python
+
+        from presidiom_image_redactor import OpenCVQRRecognizer
+
+        image = cv2.imread("qrcode.jpg")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        rec = OpenCVQRRecongnizer()
+        recognized = rec.recognize(image)
+    """
     def __init__(self) -> None:
         self.detector = cv2.QRCodeDetector()
 
     def recognize(self, image) -> List[QRRecognizerResult]:
         if not isinstance(image, np.ndarray):
-            image = np.array(image)
+            image = np.array(image, dtype=np.uint8)
 
         recognized = []
 
         ret, points = self._detect(image)
-               
-        if ret:
-            text = self._decode(image, points)
 
-            for p in points:
+        if ret:
+            decoded = self._decode(image, points)
+
+            for text, p in zip(decoded, points):
                 (x, y, w, h) = cv2.boundingRect(p)
 
                 recognized.append(QRRecognizerResult(
@@ -68,12 +83,20 @@ class OpenCVQRRecongnizer(QRRecognizer):
 
     def _detect(self, image) -> Tuple[float, Optional[np.ndarray]]:
         ret, points = self.detector.detect(image)
+
+        if not ret:
+            ret, points = self.detector.detectMulti(image)
+
         if points is not None:
             points = points.astype(int)
 
         return ret, points
 
-    def _decode(self, image, points) -> str:
-        res = self.detector.decode(image, points)
+    def _decode(self, image, points) -> Tuple[str]:
+        if len(points) == 1:
+            decoded, _ = self.detector.decode(image, points)
+            decoded = (decoded, )
+        else:
+            _, decoded, _ = self.detector.decodeMulti(image, points)
 
-        return res[0]
+        return decoded
