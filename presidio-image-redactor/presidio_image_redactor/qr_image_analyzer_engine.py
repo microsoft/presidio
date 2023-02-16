3c3fee9c29793f5584cc233f6063482f4631e927
Victor <55928886+vpvpvpvp@users.noreply.github.com>
2023-02-17 01:49:16 +0300

    Create QRImageAnalyzerEngine

---
+++ presidio-image-redactor/presidio_image_redactor/qr_image_analyzer_engine.py
@@ -0,0 +1,62 @@
+from typing import List, Optional
+
+from presidio_analyzer import AnalyzerEngine
+
+from presidio_image_redactor.entities import ImageRecognizerResult
+from presidio_image_redactor.qr_recognizer import QRRecognizer
+from presidio_image_redactor.qr_recognizer import OpenCVQRRecongnizer
+
+
+class QRImageAnalyzerEngine:
+    """QRImageAnalyzerEngine class.
+
+    :param analyzer_engine: The Presidio AnalyzerEngine instance
+        to be used to detect PII in text
+    :param qr: the QRRecognizer object to detect and decode text in qr codes
+    """
+
+    def __init__(
+        self,
+        analyzer_engine: Optional[AnalyzerEngine] = None,
+        qr: Optional[QRRecognizer] = None,
+    ):
+        if not analyzer_engine:
+            analyzer_engine = AnalyzerEngine()
+        self.analyzer_engine = analyzer_engine
+
+        if not qr:
+            qr = OpenCVQRRecongnizer()
+        self.qr = qr
+
+    def analyze(
+        self, image: object, **text_analyzer_kwargs
+    ) -> List[ImageRecognizerResult]:
+        """Analyse method to analyse the given image.
+
+        :param image: PIL Image/numpy array to be processed.
+        :param text_analyzer_kwargs: Additional values for the analyze method
+        in AnalyzerEngine.
+
+        :return: List of the extract entities with image bounding boxes.
+        """
+        bboxes = []
+
+        qr_result = self.qr.recognize(image)
+        for qr_code in qr_result:
+            analyzer_result = self.analyzer_engine.analyze(
+                text=qr_code.text, language="en", **text_analyzer_kwargs
+            )
+            for res in analyzer_result:
+                bboxes.append(
+                    ImageRecognizerResult(
+                        res.entity_type,
+                        res.start,
+                        res.end,
+                        res.score,
+                        qr_code.bbox[0],
+                        qr_code.bbox[1],
+                        qr_code.bbox[2],
+                        qr_code.bbox[3],
+                    )
+                )
+        return bboxes
