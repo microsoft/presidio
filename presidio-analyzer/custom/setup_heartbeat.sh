485834c7d490d658f9cfecca5d0ebd3aa12ad0c5
Nikhil Wadhwa <44419564+nikhilwadhwa84@users.noreply.github.com>
2023-02-27 17:26:28 -0800

    Change to script 1

--- presidio-analyzer/custom/setup_heartbeat.sh
+++ presidio-analyzer/custom/setup_heartbeat.sh
@@ -1,5 +1,7 @@
 
 export HB_PORT=21230
+echo Heartbeat port: $HB_PORT
+
 function hasHeartbeat {
   c=$(netstat -an | grep ${HB_PORT} | grep -i ESTABLISHED)
   if [[ -n $c ]]; then
@@ -10,39 +12,53 @@ function hasHeartbeat {
 
 }
 
+function hasListener {
+  c=$(netstat -an | grep ${HB_PORT} | grep -i LISTEN)
+  if [[ -n $c ]]; then
+    echo yes
+  else
+    echo no
+  fi
+}
+
 function heartbeat_loop {
   pkill nc
   sleep 1
-  #nc -k -l -p ${HB_PORT} > /dev/null 2>&1 &            //different linux version?
-  #nc -k -l ${HB_PORT} > /dev/null 2>&1 &             // dont send to standard output
-  nc -k -l -d ${HB_PORT} &
+  while :; do sleep 1; done | nc -l ${HB_PORT} > /dev/null 2>&1 &
 
-  #30 minute wait time
-  #let l=1800
-  let l=20
+  # 5 Second wait before proceeding
+  echo 5 second sleep...
+  sleep 5
 
-  while (( l-- > 0 ))
+
+  # 30 minute wait before terminating Presidio container
+  time_remaining=1800
+  while (( time_remaining-- > 0 ))
   do
-    echo wait for heartbeat $l
+    echo Wait for Heartbeat. Time remaining: "$time_remaining"
     [[ $(hasHeartbeat) == yes  ]] && {
       break;
+    }
+    [[ $(hasListener) == no && $(hasHeartbeat) == no  ]] && {
+      echo Heartbeat unavailable. hasListener: "$hasListener" hasHeartbeat: "$hasHeartbeat"
+      return 1
     }
     sleep 1
   done
 
-  if (( l <= 0 ))
+  if (( time_remaining <= 0 ))
   then
-    echo heartbeat connection timeout.
+    echo Heartbeat connection timeout.
     return 1
   fi
 
-  echo heartbeat connected.
+  echo Heartbeat connected.
 
-  l=0
+  heartbeat_count=0
 
-  while (( ++l > 0 ))
+  while (( ++heartbeat_count > 0 ))
   do
-    echo heart is beating $l
+    echo Heartbeat count: "$heartbeat_count"
     [[ $(hasHeartbeat) == no  ]] && {
       break;
     }
@@ -50,12 +66,11 @@ function heartbeat_loop {
   done
 
   echo Heartbeat stopped. Terminating ...
+
   return 0
 }
 
-heartbeat_result=1
-while((heartbeat_result!=0));
-do
-  heartbeat_result=heartbeat_loop
-  sleep 1
-done
+heartbeat_loop
+RET=$?
+echo RET=$RET
+exit $RET
