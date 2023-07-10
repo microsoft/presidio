5a44b74a5b7dd10184335ade8c5ed1af649b8b11
Nile Wilson <niwilso@microsoft.com>
2023-07-10 14:34:25 -0400

    Temporarily commenting out one integration test to see effect on build pipeline hangup

--- presidio-image-redactor/tests/integration/test_dicom_image_pii_verify_engine_integration.py
+++ presidio-image-redactor/tests/integration/test_dicom_image_pii_verify_engine_integration.py
@@ -59,37 +59,37 @@ def test_verify_correctly(
     _strip_score(test_analyzer_results_formatted)
     assert all([result in expected_analyzer_results for result in test_analyzer_results_formatted])
 
-def test_eval_dicom_correctly(
-    get_mock_dicom_instance: pydicom.dataset.FileDataset,
-    get_mock_dicom_verify_results: dict,
-):
-    """Test the eval_dicom_instance function
+# def test_eval_dicom_correctly(
+#     get_mock_dicom_instance: pydicom.dataset.FileDataset,
+#     get_mock_dicom_verify_results: dict,
+# ):
+#     """Test the eval_dicom_instance function
 
-    Args:
-        mock_engine (DicomImagePiiVerifyEngine): Instantiated engine.
-        get_mock_dicom_instance (pydicom.dataset.FileDataset): Loaded DICOM.
-        get_mock_dicom_verify_results (dict): Dictionary with loaded results.
-    """
-    # Assign
-    tolerance = 50
-    ground_truth = get_mock_dicom_verify_results["ground_truth"]
-    expected_results = {
-        "all_positives": get_mock_dicom_verify_results["all_pos"],
-        "ground_truth": ground_truth,
-        "precision": 1.0,
-        "recall": 1.0,
-    }
+#     Args:
+#         mock_engine (DicomImagePiiVerifyEngine): Instantiated engine.
+#         get_mock_dicom_instance (pydicom.dataset.FileDataset): Loaded DICOM.
+#         get_mock_dicom_verify_results (dict): Dictionary with loaded results.
+#     """
+#     # Assign
+#     tolerance = 50
+#     ground_truth = get_mock_dicom_verify_results["ground_truth"]
+#     expected_results = {
+#         "all_positives": get_mock_dicom_verify_results["all_pos"],
+#         "ground_truth": ground_truth,
+#         "precision": 1.0,
+#         "recall": 1.0,
+#     }
 
-    # Act
-    test_image, test_eval_results = DicomImagePiiVerifyEngine().eval_dicom_instance(
-        get_mock_dicom_instance, ground_truth, PADDING_WIDTH, tolerance, True
-    )
+#     # Act
+#     test_image, test_eval_results = DicomImagePiiVerifyEngine().eval_dicom_instance(
+#         get_mock_dicom_instance, ground_truth, PADDING_WIDTH, tolerance, True
+#     )
 
-    # Assert
-    assert type(test_image) == PIL.Image.Image
-    _strip_score(test_eval_results['all_positives'])
-    _strip_score(expected_results['all_positives'])
-    assert test_eval_results == expected_results
+#     # Assert
+#     assert type(test_image) == PIL.Image.Image
+#     _strip_score(test_eval_results['all_positives'])
+#     _strip_score(expected_results['all_positives'])
+#     assert test_eval_results == expected_results
 
-def _strip_score(analyzer_results):
-    [result.pop('score') for result in analyzer_results]
+# def _strip_score(analyzer_results):
+#     [result.pop('score') for result in analyzer_results]
