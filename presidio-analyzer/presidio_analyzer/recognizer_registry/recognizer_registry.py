f03e0acba2df815cd5f7f132ceb49a11017459f2
Chenghao Mou <mouchenghao@gmail.com>
2023-09-15 14:39:00 +0100

    update type check

--- presidio-analyzer/presidio_analyzer/recognizer_registry/recognizer_registry.py
+++ presidio-analyzer/presidio_analyzer/recognizer_registry/recognizer_registry.py
@@ -125,7 +125,7 @@ class RecognizerRegistry:
     ) -> Union[Type[SpacyRecognizer], Type[StanzaRecognizer]]:
         """Return the recognizer leveraging the selected NLP Engine."""
 
-        if not nlp_engine or type(nlp_engine) is SpacyNlpEngine:
+        if not nlp_engine or isinstance(nlp_engine, SpacyNlpEngine):
             return SpacyRecognizer
         if isinstance(nlp_engine, StanzaNlpEngine):
             return StanzaRecognizer
