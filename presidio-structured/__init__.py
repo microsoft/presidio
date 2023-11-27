29f7f8a16f01390ba6b9d800adbe5651eac86162
Jakob Serlier <37184788+Jakob-98@users.noreply.github.com>
2023-11-27 11:34:38 +0100

    README

--- presidio-structured/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Anonymizer root module."""
-import logging
-
-# Set up default logging (with NullHandler)
-
-logging.getLogger("presidio-structured").addHandler(logging.NullHandler())
--- presidio-structured/presidio_structured/__init__.py
+++ presidio-structured/presidio_structured/__init__.py
@@ -1,7 +1,12 @@
+""" presidio-structured root module. """
+import logging
+
 from .analysis_builder import JsonAnalysisBuilder, TabularAnalysisBuilder
 from .config import StructuredAnalysis
 from .data import CsvReader, JsonDataProcessor, JsonReader, PandasDataProcessor
 from .structured_engine import StructuredEngine
+
+logging.getLogger("presidio-structured").addHandler(logging.NullHandler())
 
 __all__ = [
     "StructuredEngine",
