c8dd4bb58772ed061b4021bdf1d0d143a3afaa1d
Roey <robencha@microsoft.com>
2024-02-07 02:26:05 -0800

    initial changes without tests

--- presidio-anonymizer/presidio_anonymizer/operators/custom.py
+++ presidio-anonymizer/presidio_anonymizer/operators/custom.py
@@ -1,5 +1,6 @@
 """Replaces the PII text with function result."""
 from typing import Dict
+from inspect import signature
 
 from presidio_anonymizer.operators import Operator, OperatorType
 from presidio_anonymizer.entities import InvalidParamException
@@ -16,14 +17,12 @@ class Custom(Operator):
 
     def operate(self, text: str = None, params: Dict = None) -> str:
         """:return: result of function executed on the text."""
-        new_val = params.get(self.LAMBDA)
-        return new_val(text)
+        return self._call_lambda(text, params)
 
     def validate(self, params: Dict) -> None:
         """Validate the provided function is returning a string."""
-        new_val = params.get(self.LAMBDA)
-        if callable(new_val):
-            if not type(new_val("PII")) == str:
+        if callable(params.get(self.LAMBDA)):
+            if not type(self._call_lambda("PII", params)) == str:
                 raise InvalidParamException("Function return type must be a str")
 
         else:
@@ -36,3 +35,10 @@ class Custom(Operator):
     def operator_type(self) -> OperatorType:
         """Return operator type."""
         return OperatorType.Anonymize
+
+    def _call_lambda(self, text: str = None, params: Dict = None):
+        new_val = params.get(self.LAMBDA)
+        sig = signature(new_val)
+        if len(sig.parameters) == 1:
+            return new_val(text)
+        return new_val(text, params.get("entity_type"))
