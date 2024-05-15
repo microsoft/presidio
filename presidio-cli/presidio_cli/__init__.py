8dac39781cdbb5a004cf6fe306271b60b801fb67
Sharon Hart <shhart@microsoft.com>
2024-05-15 11:02:04 +0300

    fix cli versioning

--- presidio-cli/presidio_cli/__init__.py
+++ presidio-cli/presidio_cli/__init__.py
@@ -1,8 +1,6 @@
 """A Python CLI for analyzing PII Entities with Microsoft Presidio framework."""
 
-import importlib.metadata
-
 APP_DESCRIPTION = __doc__
 SHELL_NAME = "presidio"
 
-APP_VERSION = __version__ = importlib.metadata.version("presidio-cli")
+APP_VERSION = __version__ = "0.0.8"
