37a96685df0e330d6a67f30d6da2ac090947f64c
Sharon Hart <shhart@microsoft.com>
2024-05-19 21:59:44 +0300

    PDM_VENV_WITH_PIP

--- presidio-cli/presidio_cli/__init__.py
+++ presidio-cli/presidio_cli/__init__.py
@@ -1,6 +1,8 @@
 """A Python CLI for analyzing PII Entities with Microsoft Presidio framework."""
 
+__version__ = "0.0.8"
+
 APP_DESCRIPTION = __doc__
 SHELL_NAME = "presidio"
 
-APP_VERSION = __version__ = "0.0.8"
+APP_VERSION = __version__
