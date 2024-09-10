9529025a20e3c2964b7556f6d9b8fa40d3fb1e09
RoeyBC <roey.benhaim@gmail.com>
2024-09-10 09:13:29 -0700

    fixing dependencies

--- presidio-analyzer/install_dependencies.sh
+++ presidio-analyzer/install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-poetry install --no-interaction
-
 if [ "$DEV_MODE" == "transformers" ]; then
-  poetry add torch transformers huggingface_hub
+  poetry install -E server -E transformers --no-interaction
+else
+  poetry install -E server --no-interaction
 fi
 
 poetry run python install_nlp_models.py --conf_file "$NLP_CONF_FILE"
