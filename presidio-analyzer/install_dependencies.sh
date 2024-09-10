1818573738db7fcd4c3b8a6c2bc9d764613c6b63
RoeyBC <roey.benhaim@gmail.com>
2024-09-10 13:03:51 -0700

    moved extras to env var

--- presidio-analyzer/install_dependencies.sh
+++ presidio-analyzer/install_dependencies.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "$DEV_MODE" == "transformers" ]; then
-  poetry install -E server -E transformers --no-interaction
-else
-  poetry install -E server --no-interaction
-fi
+poetry install -E server ${POETRY_EXTRAS} --no-interaction
 
 poetry run python install_nlp_models.py --conf_file "$NLP_CONF_FILE"
