6bc5a9f0e5207ac55933de5674f00f025f6363f5
Omri Mendels <omri374@users.noreply.github.com>
2024-12-31 12:44:06 +0200

    Fix docstring formatting in __init__.py

--- presidio-analyzer/presidio_analyzer/context_aware_enhancers/__init__.py
+++ presidio-analyzer/presidio_analyzer/context_aware_enhancers/__init__.py
@@ -1,4 +1,4 @@
-""" Context awareness modules."""
+"""Context awareness modules."""
 from .context_aware_enhancer import ContextAwareEnhancer
 from .lemma_context_aware_enhancer import LemmaContextAwareEnhancer
 
