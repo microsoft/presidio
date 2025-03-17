dcf1ae0359269e049c7cbe9825bdeff14698a68d
Sharon Hart <sharonh.dev@gmail.com>
2025-03-17 14:08:29 +0200

    drop blake2b (#1552)
    * drop blake2b
    
    * Update docs/anonymizer/index.md
    
    Co-authored-by: Omri Mendels <omri374@users.noreply.github.com>
    
    * Update test_anonymize_engine.py
    
    ---------
    
    Co-authored-by: Omri Mendels <omri374@users.noreply.github.com>

--- presidio-anonymizer/tests/integration/test_anonymize_engine.py
+++ presidio-anonymizer/tests/integration/test_anonymize_engine.py
@@ -158,13 +158,6 @@ def test_given_intersecting_the_same_entities_then_we_anonymize_correctly():
     # fmt: off
     "hash_type,result",
     [
-        ("blake2b",
-         '{"text": "hello world, my name is 9784349bcc3a48a6fe6e344c0701d31ee9ec1bd4. '
-         'My number is: 4c43f855362f7ab55108b2a83d50aae7cac3acec", "items": [{"start": 80, '
-         '"end": 120, "entity_type": "PHONE_NUMBER", "text": '
-         '"4c43f855362f7ab55108b2a83d50aae7cac3acec", "operator": "hash"}, '
-         '{"start": 24, "end": 64, "entity_type": "NAME", "text": '
-         '"9784349bcc3a48a6fe6e344c0701d31ee9ec1bd4", "operator": "hash"}]}'),
         ("sha256",
          '{"text": "hello world, my name is '
          '01332c876518a793b7c1b8dfaf6d4b404ff5db09b21c6627ca59710cc24f696a. '
