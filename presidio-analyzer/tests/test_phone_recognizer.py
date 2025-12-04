c3bb79cccd2c3fb6f0e42c024dd614f40af90ecd
WenwenHLF <wuwenwen1113@gmail.com>
2025-12-04 15:49:18 +0900

    Add Japanese and Chinese mobile number test cases
    This commit adds international PHONE_NUMBER test cases for Presidio Analyzer:
    
    - Japanese mobile number formats: 090/080/070
    - Chinese mobile number formats: 11-digit numbers starting with 1
    
    These tests improve international coverage for the PhoneRecognizer.

--- presidio-analyzer/tests/test_phone_recognizer.py
+++ presidio-analyzer/tests/test_phone_recognizer.py
@@ -24,6 +24,8 @@ def recognizer():
         ("_: +55 11 98456 5666", 1, ["PHONE_NUMBER"], ((3, 20), ), 0.4),
         ("Brazil: +55 11 98456 5666", 1, ["PHONE_NUMBER"], ((8, 25), ), 0.4),
         ("BR: +55 11 98456 5666", 1, ["PHONE_NUMBER"], ((4, 21), ), 0.4),
+        ("My Japanese number is 090-1234-5678", 1, ["PHONE_NUMBER"],((22, 35), ), 0.4),
+        ("My CN number is 13812345678", 1, ["PHONE_NUMBER"],((16, 27), ), 0.4),
         # fmt: on
     ],
 )
