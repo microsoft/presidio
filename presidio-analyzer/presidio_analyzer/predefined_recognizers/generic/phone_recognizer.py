0e9e5a774430ec0ab8af9894357c710021b60246
kim <83156897+kyoungbinkim@users.noreply.github.com>
2025-12-29 09:07:04 +0900

    Add Korea (KR) to default supported regions in PhoneRecognizer

--- presidio-analyzer/presidio_analyzer/predefined_recognizers/generic/phone_recognizer.py
+++ presidio-analyzer/presidio_analyzer/predefined_recognizers/generic/phone_recognizer.py
@@ -25,7 +25,7 @@ class PhoneRecognizer(LocalRecognizer):
 
     SCORE = 0.4
     CONTEXT = ["phone", "number", "telephone", "cell", "cellphone", "mobile", "call"]
-    DEFAULT_SUPPORTED_REGIONS = ("US", "UK", "DE", "FE", "IL", "IN", "CA", "BR")
+    DEFAULT_SUPPORTED_REGIONS = ("US", "UK", "DE", "FE", "IL", "IN", "CA", "BR", "KR")
 
     def __init__(
         self,
