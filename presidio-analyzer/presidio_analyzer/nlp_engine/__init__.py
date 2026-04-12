d91fa9863131bdaaa06bb2736759405bba274530
Sharon Hart <sharonh.dev@gmail.com>
2026-04-12 09:12:42 +0300

    Slim NLP Engine (#1916)
    * test slim nlp
    
    * add slim_nlp.yaml
    
    * use blank spacy model for tokenization
    
    * spacy generic tokenizer
    
    * Update nlp_engine_provider.py
    
    ---------
    
    Co-authored-by: Omri Mendels <omri374@users.noreply.github.com>

--- presidio-analyzer/presidio_analyzer/nlp_engine/__init__.py
+++ presidio-analyzer/presidio_analyzer/nlp_engine/__init__.py
@@ -4,6 +4,7 @@ from .device_detector import device_detector
 from .ner_model_configuration import NerModelConfiguration
 from .nlp_artifacts import NlpArtifacts
 from .nlp_engine import NlpEngine
+from .slim_spacy_nlp_engine import SlimSpacyNlpEngine
 from .spacy_nlp_engine import SpacyNlpEngine
 from .stanza_nlp_engine import StanzaNlpEngine
 from .transformers_nlp_engine import TransformersNlpEngine
@@ -15,6 +16,7 @@ __all__ = [
     "NerModelConfiguration",
     "NlpArtifacts",
     "NlpEngine",
+    "SlimSpacyNlpEngine",
     "SpacyNlpEngine",
     "StanzaNlpEngine",
     "NlpEngineProvider",
